2a9c5221133bb0a94c1aed083a95020cac62fd0a
zhanghongtong <rory-z@outlook.com>
2021-11-19 15:38:09 +0800

    ci: add deploy emqx-ee workflows

--- controllers/ensure.go
+++ controllers/ensure.go
@@ -6,29 +6,28 @@ import (
 )
 
 // Ensure the EMQ X Cluster's components are correct.
-func (ech *EmqxBrokerClusterHandler) Ensure(emqx v1alpha2.Emqx, labels map[string]string, or []metav1.OwnerReference) error {
-
-	if err := ech.eService.EnsureEmqxBrokerSecret(emqx, labels, or); err != nil {
+func (ech *EmqxBrokerClusterHandler) Ensure(emqx v1alpha2.Emqx, labels map[string]string, ownerRefs []metav1.OwnerReference) error {
+	if err := ech.eService.EnsureEmqxBrokerSecret(emqx, labels, ownerRefs); err != nil {
 		return err
 	}
 
-	if err := ech.eService.EnsureEmqxBrokerHeadlessService(emqx, labels, or); err != nil {
+	if err := ech.eService.EnsureEmqxBrokerHeadlessService(emqx, labels, ownerRefs); err != nil {
 		return err
 	}
 
-	if err := ech.eService.EnsureEmqxBrokerConfigMapForAcl(emqx, labels, or); err != nil {
+	if err := ech.eService.EnsureEmqxBrokerConfigMapForAcl(emqx, labels, ownerRefs); err != nil {
 		return err
 	}
 
-	if err := ech.eService.EnsureEmqxBrokerConfigMapForLoadedModules(emqx, labels, or); err != nil {
+	if err := ech.eService.EnsureEmqxBrokerConfigMapForLoadedModules(emqx, labels, ownerRefs); err != nil {
 		return err
 	}
 
-	if err := ech.eService.EnsureEmqxBrokerConfigMapForLoadedPlugins(emqx, labels, or); err != nil {
+	if err := ech.eService.EnsureEmqxBrokerConfigMapForLoadedPlugins(emqx, labels, ownerRefs); err != nil {
 		return err
 	}
 
-	if err := ech.eService.EnsureEmqxBrokerStatefulSet(emqx, labels, or); err != nil {
+	if err := ech.eService.EnsureEmqxBrokerStatefulSet(emqx, labels, ownerRefs); err != nil {
 		return err
 	}
 
