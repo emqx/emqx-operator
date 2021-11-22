76f00b5b72842c259dbca6d921f6a40e947da2a9
kiragoo <zhangl910201@gmail.com>
2021-11-22 11:20:19 +0800

    feat(test-unittest): unit test template init in generator

---
+++ pkg/service/generator_test.go
@@ -0,0 +1,50 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/emqx/emqx-operator/api/v1alpha2"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var (
+	replicas int32 = 3
+	emqx           = &v1alpha2.EmqxBroker{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "emqx",
+			Namespace: "emqx-system",
+		},
+		Spec: v1alpha2.EmqxBrokerSpec{
+			Replicas:           &replicas,
+			Image:              "emqx/emqx:4.3.10",
+			ServiceAccountName: "emqx",
+			// Storage: &{},
+			// AclConf:
+			// 	"{allow, {user, \"dashboard\"}, subscribe, [\"$SYS/#\"]}.
+			// 	{allow, {ipaddr, \"127.0.0.1\"}, pubsub, [\"$SYS/#\", \"#\"]}.
+			// 	{deny, all, subscribe, [\"$SYS/#\", {eq, \"#\"}]}.
+			// 	{allow, all}."
+			// LoadedPluginConf: {},
+			// LoadedModulesConf: {},
+		},
+	}
+)
+
+// TODO
+// unit test for newConfigMapForAcl
+func TestnewConfigMapForAcl(t *testing.T) {
+
+	// type args struct {
+	// 	t    *v1alpha2.Emqx
+	// 	data map[string]string
+	// }
+
+	// TODO
+	// tests := []struct {
+	// 	ns string
+	// 	arg args
+	// 	want string
+	// }{
+
+	// }
+}
