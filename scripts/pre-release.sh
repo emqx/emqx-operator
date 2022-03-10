c2103ff887380afe211389e8f06dc713065237e0
kiragoo <zhangl910201@gmail.com>
2022-03-10 19:25:43 +0800

    fix(script): fix the scrpit run on macos error

--- scripts/pre-release.sh
+++ scripts/pre-release.sh
@@ -11,6 +11,13 @@ KUSTOMIZE=${PWD}/bin/kustomize
 pushd config/manager && ${KUSTOMIZE} edit set image controller="emqx/emqx-operator-controller:${tag}" && popd
 ${KUSTOMIZE} build config/default > deploy/manifests/emqx-operator-controller.yaml
 
-sed -i "s|https://github.com/emqx/emqx-operator/releases/download/.*/emqx-operator-controller.yaml|https://github.com/emqx/emqx-operator/releases/download/${tag}/emqx-operator-controller.yaml|g" docs/en_US/getting-started/getting-started.md
-sed -i "s|https://github.com/emqx/emqx-operator/releases/download/.*/emqx-operator-controller.yaml|https://github.com/emqx/emqx-operator/releases/download/${tag}/emqx-operator-controller.yaml|g" docs/zh_CN/getting-started/getting-started.md
-sed -i -r "s|^appVersion:.*|appVersion: ${tag}|g" deploy/charts/emqx-operator/Chart.yaml
+# Default case for Linux sed,just use "-i"
+sedi=(-i)
+case "$(uname)" in
+    # For macOS, use two parameters
+    Darwin*) sedi=(-i "")
+esac
+
+sed "${sedi[@]}" "s|https://github.com/emqx/emqx-operator/releases/download/.*/emqx-operator-controller.yaml|https://github.com/emqx/emqx-operator/releases/download/${tag}/emqx-operator-controller.yaml|g" docs/en_US/getting-started/getting-started.md
+sed "${sedi[@]}" "s|https://github.com/emqx/emqx-operator/releases/download/.*/emqx-operator-controller.yaml|https://github.com/emqx/emqx-operator/releases/download/${tag}/emqx-operator-controller.yaml|g" docs/zh_CN/getting-started/getting-started.md
+sed "${sedi[@]}" -r "s|^appVersion:.*|appVersion: ${tag}|g" deploy/charts/emqx-operator/Chart.yaml
