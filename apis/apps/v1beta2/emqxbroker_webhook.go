d614092a7faefbcf0e9658d95d33e144ed65a7d8
zhanghongtong <rory-z@outlook.com>
2022-03-16 18:54:50 +0800

    fix(webhook): fix wrong admissionReviewVersions

--- apis/apps/v1beta2/emqxbroker_webhook.go
+++ apis/apps/v1beta2/emqxbroker_webhook.go
@@ -38,7 +38,7 @@ func (r *EmqxBroker) SetupWebhookWithManager(mgr ctrl.Manager) error {
 		Complete()
 }
 
-//+kubebuilder:webhook:path=/mutate-apps-emqx-io-v1beta2-emqxbroker,mutating=true,failurePolicy=fail,sideEffects=None,groups=apps.emqx.io,resources=emqxbrokers,verbs=create;update,versions=v1beta2,name=mutating.emqxbroker.v1beta2.emqx.io,admissionReviewVersions={v1,v1beta2}
+//+kubebuilder:webhook:path=/mutate-apps-emqx-io-v1beta2-emqxbroker,mutating=true,failurePolicy=fail,sideEffects=None,groups=apps.emqx.io,resources=emqxbrokers,verbs=create;update,versions=v1beta2,name=mutating.emqxbroker.v1beta2.emqx.io,admissionReviewVersions={v1,v1beta1}
 
 var _ webhook.Defaulter = &EmqxBroker{}
 
@@ -121,7 +121,7 @@ func (r *EmqxBroker) Default() {
 	}
 }
 
-//+kubebuilder:webhook:path=/validate-apps-emqx-io-v1beta2-emqxbroker,mutating=false,failurePolicy=fail,sideEffects=None,groups=apps.emqx.io,resources=emqxbrokers,verbs=create;update,versions=v1beta2,name=validator.emqxbroker.v1beta2.emqx.io,admissionReviewVersions={v1,v1beta2}
+//+kubebuilder:webhook:path=/validate-apps-emqx-io-v1beta2-emqxbroker,mutating=false,failurePolicy=fail,sideEffects=None,groups=apps.emqx.io,resources=emqxbrokers,verbs=create;update,versions=v1beta2,name=validator.emqxbroker.v1beta2.emqx.io,admissionReviewVersions={v1,v1beta1}
 
 var _ webhook.Validator = &EmqxBroker{}
 
