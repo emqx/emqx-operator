3a675fccdbe2a01537e3e313c1a9f3740e58539b
Rory Z <rory-z@outlook.com>
2022-04-19 16:51:22 +0800

    test(webhook): delete needless test case

--- test/webhooks/defaulting_test.go
+++ test/webhooks/defaulting_test.go
@@ -15,90 +15,52 @@ import (
 
 var _ = Describe("EMQX Broker", func() {
 	Context("Check EMQX Broker", func() {
-		AfterEach(func() {
-			Expect(k8sClient.Delete(
-				context.Background(),
-				&v1beta3.EmqxBroker{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "broker",
-						Namespace: "default",
-					},
+		v1beta3EmqxBroker := &v1beta3.EmqxBroker{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "broker",
+				Namespace: "default",
+				Labels: map[string]string{
+					"foo": "bar",
 				},
-			)).Should(Succeed())
+			},
+			Spec: v1beta3.EmqxBrokerSpec{
+				Image: "emqx/emqx:4.4.3",
+			},
+		}
+		BeforeEach(func() {
+			Expect(k8sClient.Create(context.Background(), v1beta3EmqxBroker)).Should(Succeed())
+		})
+		AfterEach(func() {
+			Expect(k8sClient.Delete(context.Background(), v1beta3EmqxBroker)).Should(Succeed())
 		})
 		It("Check defaulting", func() {
-			v1beta3EmqxBroker := &v1beta3.EmqxBroker{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "broker",
-					Namespace: "default",
-				},
-			}
-			Expect(k8sClient.Create(context.Background(), v1beta3EmqxBroker)).Should(Succeed())
 			checkDefaulting(v1beta3EmqxBroker)
-		})
-		It("Check defaulting with telegraf", func() {
-			v1beta3EmqxBroker := &v1beta3.EmqxBroker{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "broker",
-					Namespace: "default",
-				},
-			}
-
-			telegrafConf := `[global_tags]
-			instanceID = "test"
-			[[outputs.discard]]`
-
-			v1beta3EmqxBroker.Spec.TelegrafTemplate = &v1beta3.TelegrafTemplate{
-				Image: "telegraf:1.19.3",
-				Conf:  &telegrafConf,
-			}
-			Expect(k8sClient.Create(context.Background(), v1beta3EmqxBroker)).Should(Succeed())
-			checkDefaultingWithTelegraf(v1beta3EmqxBroker)
 		})
 	})
 })
 
 var _ = Describe("EMQX Enterprise", func() {
 	Context("Check EMQX Enterprise", func() {
-		AfterEach(func() {
-			Expect(k8sClient.Delete(
-				context.Background(),
-				&v1beta3.EmqxEnterprise{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "enterprise",
-						Namespace: "default",
-					},
+		v1beta3EmqxEnterprise := &v1beta3.EmqxEnterprise{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "enterprise",
+				Namespace: "default",
+				Labels: map[string]string{
+					"foo": "bar",
 				},
-			)).Should(Succeed())
+			},
+			Spec: v1beta3.EmqxEnterpriseSpec{
+				Image: "emqx/emqx-ee:4.4.3",
+			},
+		}
+		BeforeEach(func() {
+			Expect(k8sClient.Create(context.Background(), v1beta3EmqxEnterprise)).Should(Succeed())
+		})
+		AfterEach(func() {
+			Expect(k8sClient.Delete(context.Background(), v1beta3EmqxEnterprise)).Should(Succeed())
 		})
 		It("Check defaulting", func() {
-			v1beta3EmqxEnterprise := &v1beta3.EmqxEnterprise{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "enterprise",
-					Namespace: "default",
-				},
-			}
-			Expect(k8sClient.Create(context.Background(), v1beta3EmqxEnterprise)).Should(Succeed())
 			checkDefaulting(v1beta3EmqxEnterprise)
-		})
-		It("Check defaulting with telegraf", func() {
-			v1beta3EmqxEnterprise := &v1beta3.EmqxEnterprise{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "enterprise",
-					Namespace: "default",
-				},
-			}
-
-			telegrafConf := `[global_tags]
-			instanceID = "test"
-			[[outputs.discard]]`
-
-			v1beta3EmqxEnterprise.Spec.TelegrafTemplate = &v1beta3.TelegrafTemplate{
-				Image: "telegraf:1.19.3",
-				Conf:  &telegrafConf,
-			}
-			Expect(k8sClient.Create(context.Background(), v1beta3EmqxEnterprise)).Should(Succeed())
-			checkDefaultingWithTelegraf(v1beta3EmqxEnterprise)
 		})
 	})
 })
@@ -116,6 +78,7 @@ func checkDefaulting(emqx v1beta3.Emqx) {
 		return err
 	}, timeout, interval).Should(Succeed())
 
+	Expect(emqx.GetLabels()).Should(HaveKeyWithValue("foo", "bar"))
 	Expect(emqx.GetLabels()).Should(HaveKeyWithValue("apps.emqx.io/managed-by", "emqx-operator"))
 	Expect(emqx.GetLabels()).Should(HaveKeyWithValue("apps.emqx.io/instance", emqx.GetName()))
 
@@ -126,38 +89,7 @@ func checkDefaulting(emqx v1beta3.Emqx) {
 		ContainElements(
 			corev1.EnvVar{
 				Name:  "EMQX_CLUSTER__DISCOVERY",
-				Value: "k8s",
-			},
-		),
-	)
-}
-
-func checkDefaultingWithTelegraf(emqx v1beta3.Emqx) {
-	Eventually(func() error {
-		err := k8sClient.Get(
-			context.TODO(),
-			types.NamespacedName{
-				Name:      emqx.GetName(),
-				Namespace: emqx.GetNamespace(),
-			},
-			emqx,
-		)
-		return err
-	}, timeout, interval).Should(Succeed())
-
-	Expect(emqx.GetPlugins()).Should(
-		ContainElements(
-			v1beta3.Plugin{
-				Name:   "emqx_prometheus",
-				Enable: true,
-			},
-		),
-	)
-	Expect(emqx.GetEnv()).Should(
-		ContainElements(
-			corev1.EnvVar{
-				Name:  "EMQX_PROMETHEUS__PUSH__GATEWAY__SERVER",
-				Value: "",
+				Value: "dns",
 			},
 		),
 	)
