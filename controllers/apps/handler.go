3f7ad3e8b10e63ec0d8b0b0a505fa6762ab2cf89
Rory Z <rory-z@outlook.com>
2022-05-05 17:36:56 +0800

    chore: delete cache.go and event.go

--- controllers/apps/handler.go
+++ controllers/apps/handler.go
@@ -2,13 +2,13 @@ package apps
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/emqx/emqx-operator/apis/apps/v1beta3"
-	"github.com/emqx/emqx-operator/pkg/cache"
 	"github.com/emqx/emqx-operator/pkg/manager"
 	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	mgr "sigs.k8s.io/controller-runtime/pkg/manager"
@@ -23,94 +23,31 @@ type EmqxHandler interface {
 type Handler struct {
 	client    client.Client
 	executor  manager.Executor
-	eventsCli manager.Event
+	eventsCli record.EventRecorder
 	logger    logr.Logger
-	metaCache *cache.MetaMap
 }
 
 func NewHandler(mgr mgr.Manager) *Handler {
 	return &Handler{
 		client:    mgr.GetClient(),
 		executor:  *manager.NewExecutor(mgr.GetConfig()),
-		metaCache: new(cache.MetaMap),
-		eventsCli: manager.NewEvent(mgr.GetEventRecorderFor("emqx-operator")),
+		eventsCli: mgr.GetEventRecorderFor("emqx-operator"),
 		logger:    log,
 	}
 }
 
 // Do will ensure the EMQX Cluster is in the expected state and update the EMQX Cluster status.
 func (handler *Handler) Do(emqx v1beta3.Emqx) error {
-	// diff new and new EMQX Cluster, then update status
-	meta := handler.metaCache.Cache(emqx)
-	handler.logger.WithValues("namespace", emqx.GetNamespace(), "name", emqx.GetName()).V(3).
-		Info(fmt.Sprintf("meta status:%s, mes:%s, state:%s", meta.Status, meta.Message, meta.State))
-	if err := handler.updateStatus(meta); err != nil {
-		return err
-	}
-
 	handler.logger.WithValues("namespace", emqx.GetNamespace(), "name", emqx.GetName()).V(2).Info("Ensure...")
-	handler.eventsCli.EnsureCluster(emqx)
-	if err := handler.Ensure(meta.Obj); err != nil {
-		handler.eventsCli.FailedCluster(emqx, err.Error())
+	if err := handler.Ensure(emqx); err != nil {
+		handler.eventsCli.Event(emqx, corev1.EventTypeWarning, "Ensure", err.Error())
 		emqx.SetFailedCondition(err.Error())
 		_ = handler.updateEmqxStatus(emqx)
-		// TODO
-		// metrics.ClusterMetrics.SetClusterError(emqx.GetNamespace(), emqx.GetName())
 		return err
 	}
 
-	handler.logger.WithValues("namespace", emqx.GetNamespace(), "name", emqx.GetName()).V(2).Info("CheckAndHeal...")
-	handler.eventsCli.CheckCluster(emqx)
-
-	// TODO
-	// if err := handler.CheckAndHeal(meta); err != nil {
-	// 	metrics.ClusterMetrics.SetClusterError(emqx.GetNamespace(), emqx.GetName())
-	// 	if err.Error() != needRequeueMsg {
-	// 		handler.eventsCli.FailedCluster(emqx, err.Error())
-	// 		handler.Status.SetFailedCondition(err.Error())
-	// 		handler.client.UpdateCluster(emqx.GetNamespace(), emqx)
-	// 		return err
-	// 	}
-	// 	// if user delete statefulset or deployment, set status
-	// 	status := emqx.Status.Conditions
-	// 	if len(status) > 0 && status[0].Type == v1beta3.ClusterConditionHealthy {
-	// 		handler.eventsCli.CreateCluster(emqx)
-	// 		handler.Status.SetCreateCondition("emqx server be removed by user, restart")
-	// 		handler.client.UpdateCluster(emqx.GetNamespace(), emqx)
-	// 	}
-	// 	return err
-	// }
-
-	handler.logger.WithValues("namespace", emqx.GetNamespace(), "name", emqx.GetName()).V(2).Info("SetReadyCondition...")
-	handler.eventsCli.HealthCluster(emqx)
 	emqx.SetReadyCondition("Cluster ok")
 	return handler.updateEmqxStatus(emqx)
-}
-
-func (handler *Handler) updateStatus(meta *cache.Meta) error {
-	emqx := meta.Obj
-
-	if meta.State != cache.Check {
-		switch meta.Status {
-		case v1beta3.ClusterConditionCreating:
-			handler.eventsCli.CreateCluster(emqx)
-			emqx.SetCreateCondition(meta.Message)
-		case v1beta3.ClusterConditionScaling:
-			handler.eventsCli.NewNodeAdd(emqx, meta.Message)
-			emqx.SetScalingUpCondition(meta.Message)
-		case v1beta3.ClusterConditionScalingDown:
-			handler.eventsCli.NodeRemove(emqx, meta.Message)
-			emqx.SetScalingDownCondition(meta.Message)
-		case v1beta3.ClusterConditionUpgrading:
-			handler.eventsCli.UpdateCluster(emqx, meta.Message)
-			emqx.SetUpgradingCondition(meta.Message)
-		default:
-			handler.eventsCli.UpdateCluster(emqx, meta.Message)
-			emqx.SetUpdatingCondition(meta.Message)
-		}
-		return handler.updateEmqxStatus(emqx)
-	}
-	return nil
 }
 
 func (handler *Handler) updateEmqxStatus(emqx v1beta3.Emqx) error {
