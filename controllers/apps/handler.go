0f8b6b6f29726d27f45d3b65c07c408b5d5c2cdc
Rory Z <rory-z@outlook.com>
2022-05-07 14:35:55 +0800

    chore: delete do function for handler

--- controllers/apps/handler.go
+++ controllers/apps/handler.go
@@ -6,7 +6,6 @@ import (
 	"github.com/emqx/emqx-operator/apis/apps/v1beta3"
 	"github.com/emqx/emqx-operator/pkg/manager"
 	"github.com/go-logr/logr"
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -34,41 +33,4 @@ func NewHandler(mgr mgr.Manager) *Handler {
 		eventsCli: mgr.GetEventRecorderFor("emqx-operator"),
 		logger:    log,
 	}
-}
-
-// Do will ensure the EMQX Cluster is in the expected state and update the EMQX Cluster status.
-func (handler *Handler) Do(emqx v1beta3.Emqx) error {
-	handler.logger.WithValues("namespace", emqx.GetNamespace(), "name", emqx.GetName()).V(2).Info("Ensure...")
-	if err := handler.Ensure(emqx); err != nil {
-		handler.eventsCli.Event(emqx, corev1.EventTypeWarning, "Ensure", err.Error())
-		emqx.SetFailedCondition(err.Error())
-		_ = handler.updateEmqxStatus(emqx)
-		return err
-	}
-
-	emqx.SetRunningCondition("Cluster ok")
-	return handler.updateEmqxStatus(emqx)
-}
-
-func (handler *Handler) updateEmqxStatus(emqx v1beta3.Emqx) error {
-	emqx.DescConditionsByTime()
-	err := handler.client.Status().Update(context.TODO(), emqx)
-	if err != nil {
-		handler.logger.WithValues(
-			"kind", emqx.GetKind(),
-			"apiVersion", emqx.GetAPIVersion(),
-			"namespace", emqx.GetNamespace(),
-			"name", emqx.GetName(),
-			"conditions", emqx.GetConditions(),
-		).Error(err, "Update emqx broker status unsuccessfully")
-		return err
-	}
-	handler.logger.WithValues(
-		"kind", emqx.GetKind(),
-		"apiVersion", emqx.GetAPIVersion(),
-		"namespace", emqx.GetNamespace(),
-		"name", emqx.GetName(),
-		"conditions", emqx.GetConditions(),
-	).V(3).Info("Update emqx broker status successfully")
-	return nil
 }
