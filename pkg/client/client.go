703a5cde5c0ac512139759153eb618d03a0bc727
bin <brian.xb.bian@gmail.com>
2022-06-02 07:29:53 +0800

    fix: rebase from main

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -10,7 +10,6 @@ import (
 )
 
 const (
-	TIMEOUT     = 10
 	API_VERSION = "api/v4"
 )
 
@@ -18,11 +17,6 @@ type EmqxAPI struct {
 	id     string
 	passwd string
 	url    string
-}
-
-type BrokersResp struct {
-	Data []interface{} `json:"data"`
-	Code int           `json:"code"`
 }
 
 func New(url, id, passwd string) *EmqxAPI {
@@ -33,28 +27,28 @@ func New(url, id, passwd string) *EmqxAPI {
 	}
 }
 
-func (e *EmqxAPI) Get(resource string) ([]byte, error) {
+func (e *EmqxAPI) Get(resource string, timeout time.Duration) ([]byte, error) {
 	u, err := url.Parse(e.url)
 	if err != nil {
-		return nil, fmt.Errorf("Got error %s", err.Error())
+		return nil, fmt.Errorf("parse error: %s", err.Error())
 	}
 	u.Path = path.Join(u.Path, API_VERSION, resource)
 
 	req, err := http.NewRequest("GET", u.String(), nil)
 	if err != nil {
-		return nil, fmt.Errorf("Got error %s", err.Error())
+		return nil, fmt.Errorf("new request error: %s", err.Error())
 	}
 	req.SetBasicAuth(e.id, e.passwd)
 
-	c := http.Client{Timeout: TIMEOUT * time.Second}
+	c := http.Client{Timeout: timeout * time.Second}
 	resp, err := c.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("Got error: %s", err.Error())
+		return nil, fmt.Errorf("http client error: %s", err.Error())
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, fmt.Errorf("Got error: %s", err.Error())
+		return nil, fmt.Errorf("read body error: %s", err.Error())
 	}
 	return body, nil
 }
--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -1,39 +1,40 @@
 package client_test
 
 import (
-	// "net/http"
-	// "net/http/httptest"
-
-	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/tidwall/gjson"
 
 	"github.com/emqx/emqx-operator/pkg/client"
 )
 
-const (
-	expected = `{"data":[{"version":"4.4.3","uptime":"15 minutes, 1 seconds","sysdescr":"EMQ X Enterprise","otp_release":"24.1.5/12.1.5","node_status":"Running","node":"emqx@10.0.29.133","datetime":"2022-05-25 07:34:13"},{"version":"4.4.3","uptime":"15 minutes, 11 seconds","sysdescr":"EMQ X Enterprise","otp_release":"24.1.5/12.1.5","node_status":"Running","node":"emqx@10.0.17.90","datetime":"2022-05-25 07:34:13"},{"version":"4.4.3","uptime":"14 minutes, 37 seconds","sysdescr":"EMQ X Enterprise","otp_release":"24.1.5/12.1.5","node_status":"Running","node":"emqx@10.0.30.111","datetime":"2022-05-25 07:34:13"}],"code":0}`
-)
-
 func TestGetBrokers(t *testing.T) {
+	const (
+		expected = `{"data":[{"version":"4.4.3","uptime":"15 minutes, 1 seconds","sysdescr":"EMQ X Enterprise","otp_release":"24.1.5/12.1.5","node_status":"Running","node":"emqx@10.0.29.133","datetime":"2022-05-25 07:34:13"},{"version":"4.4.3","uptime":"15 minutes, 11 seconds","sysdescr":"EMQ X Enterprise","otp_release":"24.1.5/12.1.5","node_status":"Running","node":"emqx@10.0.17.90","datetime":"2022-05-25 07:34:13"},{"version":"4.4.3","uptime":"14 minutes, 37 seconds","sysdescr":"EMQ X Enterprise","otp_release":"24.1.5/12.1.5","node_status":"Running","node":"emqx@10.0.30.111","datetime":"2022-05-25 07:34:13"}],"code":0}`
+	)
+
+	type BrokersResp struct {
+		Data []interface{} `json:"data"`
+		Code int           `json:"code"`
+	}
+
 	// Start a local HTTP server
 	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		// Send response to be tested
-		// ignore for lint check
+		// just avoiding lint check
 		_, _ = rw.Write([]byte(expected))
 	}))
-
 	// Close the server when test finishes
 	defer server.Close()
 
 	api := client.New(server.URL, "admin", "public")
-	body, _ := api.Get("brokers")
+	data, _ := api.Get("brokers", 10)
 
-	d := client.BrokersResp{}
-	_ = json.Unmarshal(body, &d)
+	expectedArr := gjson.Get(expected, "data").Array()
+	actualArr := gjson.Get(string(data[:]), "data").Array()
 
-	assert.Equal(t, 3, len(d.Data))
+	assert.Equal(t, len(expectedArr), len(actualArr))
 }
