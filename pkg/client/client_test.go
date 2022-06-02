dad0e13cb1078a053702956654d3a123999948ba
bin <brian.xb.bian@gmail.com>
2022-06-02 08:11:28 +0800

    fix: remove a unused struct

--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -16,15 +16,9 @@ func TestGetBrokers(t *testing.T) {
 		expected = `{"data":[{"version":"4.4.3","uptime":"15 minutes, 1 seconds","sysdescr":"EMQ X Enterprise","otp_release":"24.1.5/12.1.5","node_status":"Running","node":"emqx@10.0.29.133","datetime":"2022-05-25 07:34:13"},{"version":"4.4.3","uptime":"15 minutes, 11 seconds","sysdescr":"EMQ X Enterprise","otp_release":"24.1.5/12.1.5","node_status":"Running","node":"emqx@10.0.17.90","datetime":"2022-05-25 07:34:13"},{"version":"4.4.3","uptime":"14 minutes, 37 seconds","sysdescr":"EMQ X Enterprise","otp_release":"24.1.5/12.1.5","node_status":"Running","node":"emqx@10.0.30.111","datetime":"2022-05-25 07:34:13"}],"code":0}`
 	)
 
-	type BrokersResp struct {
-		Data []interface{} `json:"data"`
-		Code int           `json:"code"`
-	}
-
 	// Start a local HTTP server
 	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		// Send response to be tested
-		// just avoiding lint check
 		_, _ = rw.Write([]byte(expected))
 	}))
 	// Close the server when test finishes
