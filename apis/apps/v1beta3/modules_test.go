7f7fe0c8d296b0aac0c6387a80aaaab00b38adeb
Rory Z <rory-z@outlook.com>
2022-07-27 11:36:05 +0800

    test: update unit test case for api

--- apis/apps/v1beta3/modules_test.go
+++ apis/apps/v1beta3/modules_test.go
@@ -1,16 +1,15 @@
-package v1beta3_test
+package v1beta3
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
-	"github.com/emqx/emqx-operator/apis/apps/v1beta3"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 func TestEmqxBrokerModulesString(t *testing.T) {
-	modules := &v1beta3.EmqxBrokerModuleList{
-		Items: []v1beta3.EmqxBrokerModule{
+	modules := &EmqxBrokerModuleList{
+		Items: []EmqxBrokerModule{
 			{
 				Name:   "foo",
 				Enable: true,
@@ -25,4 +24,32 @@ func TestEmqxBrokerModulesString(t *testing.T) {
 	assert.Equal(t, modules.String(),
 		"{foo, true}.\n{bar, false}.\n",
 	)
+}
+
+func TestEmqxEnterpriseModulesString(t *testing.T) {
+	var modules *EmqxEnterpriseModuleList
+
+	modules = &EmqxEnterpriseModuleList{}
+	assert.Equal(t, modules.String(), "")
+
+	modules = &EmqxEnterpriseModuleList{
+		Items: []EmqxEnterpriseModule{
+			{
+				Name:    "internal_acl",
+				Enable:  true,
+				Configs: runtime.RawExtension{Raw: []byte(`{"acl_rule_file": "/mounted/acl/acl.conf"}`)},
+			},
+			{
+				Name:   "retainer",
+				Enable: true,
+				Configs: runtime.RawExtension{Raw: []byte(`{
+					"expiry_interval": 0,
+					"max_payload_size": "1MB",
+					"max_retained_messages": 0,
+					"storage_type": "ram"
+				}`)},
+			},
+		},
+	}
+	assert.Equal(t, modules.String(), `[{"name":"internal_acl","enable":true,"configs":{"acl_rule_file":"/mounted/acl/acl.conf"}},{"name":"retainer","enable":true,"configs":{"expiry_interval":0,"max_payload_size":"1MB","max_retained_messages":0,"storage_type":"ram"}}]`)
 }
