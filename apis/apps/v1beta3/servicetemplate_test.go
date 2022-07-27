96acb2583129c38e8a04ed102f9668a49d326f02
Rory Z <rory-z@outlook.com>
2022-07-27 11:36:05 +0800

    test: update unit test case for api

--- apis/apps/v1beta3/servicetemplate_test.go
+++ apis/apps/v1beta3/servicetemplate_test.go
@@ -1,15 +1,14 @@
-package v1beta3_test
+package v1beta3
 
 import (
 	"testing"
 
-	"github.com/emqx/emqx-operator/apis/apps/v1beta3"
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 )
 
 func TestMergePorts(t *testing.T) {
-	serviceTemplate := &v1beta3.ServiceTemplate{
+	serviceTemplate := &ServiceTemplate{
 		Spec: corev1.ServiceSpec{
 			Ports: []corev1.ServicePort{
 				{
