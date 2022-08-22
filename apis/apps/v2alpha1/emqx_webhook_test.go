e69d83c1832b728b0575da17be84756c517b12fa
Rory Z <rory-z@outlook.com>
2022-08-22 18:28:11 +0800

    test: add more test case

--- apis/apps/v2alpha1/emqx_webhook_test.go
+++ apis/apps/v2alpha1/emqx_webhook_test.go
@@ -24,118 +24,9 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func TestDefaultBootstrapConfig(t *testing.T) {
-	t.Run("empty bootstrap config", func(t *testing.T) {
-		instance := &EMQX{
-			Spec: EMQXSpec{
-				BootstrapConfig: "",
-			},
-		}
-		err := instance.defaultBootstrapConfig()
-		assert.Nil(t, err)
-
-		bootstrapConfig, err := hocon.ParseString(instance.Spec.BootstrapConfig)
-		assert.Nil(t, err)
-
-		assert.NotNil(t, bootstrapConfig.GetString("node.cookie"))
-		assert.Equal(t, "data", bootstrapConfig.GetString("node.data_dir"))
-		assert.Equal(t, "etc", bootstrapConfig.GetString("node.etc_dir"))
-
-		assert.Equal(t, "18083", bootstrapConfig.GetString("dashboard.listeners.http.bind"))
-		assert.Equal(t, "admin", bootstrapConfig.GetString("dashboard.default_username"))
-		assert.Equal(t, "public", bootstrapConfig.GetString("dashboard.default_password"))
-
-		assert.Equal(t, "\"0.0.0.0:1883\"", bootstrapConfig.GetString("listeners.tcp.default.bind"))
-		assert.Equal(t, "1024000", bootstrapConfig.GetString("listeners.tcp.default.max_connections"))
-	})
-
-	t.Run("already set cookie", func(t *testing.T) {
-		instance := &EMQX{
-			Spec: EMQXSpec{
-				BootstrapConfig: `node.cookie = "12345"`,
-			},
-		}
-		err := instance.defaultBootstrapConfig()
-		assert.Nil(t, err)
-
-		bootstrapConfig, err := hocon.ParseString(instance.Spec.BootstrapConfig)
-		assert.Nil(t, err)
-		assert.Equal(t, "12345", bootstrapConfig.GetString("node.cookie"))
-	})
-
-	t.Run("already set listener", func(t *testing.T) {
-		instance := &EMQX{
-			Spec: EMQXSpec{
-				BootstrapConfig: `listeners.tcp.default.bind = "0.0.0.0:11883"`,
-			},
-		}
-		err := instance.defaultBootstrapConfig()
-		assert.Nil(t, err)
-
-		bootstrapConfig, err := hocon.ParseString(instance.Spec.BootstrapConfig)
-		assert.Nil(t, err)
-		assert.Equal(t, "\"0.0.0.0:11883\"", bootstrapConfig.GetString("listeners.tcp.default.bind"))
-		assert.Equal(t, "1024000", bootstrapConfig.GetString("listeners.tcp.default.max_connections"))
-	})
-
-	t.Run("other style set listener", func(t *testing.T) {
-		instance := &EMQX{
-			Spec: EMQXSpec{
-				BootstrapConfig: `
-					listeners {
-						tcp {
-							default {
-								bind = "0.0.0.0:11883"
-							}
-						}
-					}
-					`,
-			},
-		}
-		err := instance.defaultBootstrapConfig()
-		assert.Nil(t, err)
-
-		bootstrapConfig, err := hocon.ParseString(instance.Spec.BootstrapConfig)
-		assert.Nil(t, err)
-		assert.Equal(t, "\"0.0.0.0:11883\"", bootstrapConfig.GetString("listeners.tcp.default.bind"))
-		assert.Equal(t, "1024000", bootstrapConfig.GetString("listeners.tcp.default.max_connections"))
-	})
-}
-
 func TestDefault(t *testing.T) {
-	instance := &EMQX{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "webhook-test",
-			Namespace: "default",
-		},
-		Spec: EMQXSpec{
-			Image:           "emqx:latest",
-			BootstrapConfig: "for = bar",
-		},
-	}
+	instance := &EMQX{}
 	instance.Default()
-
-	assert.NotNil(t, instance.Spec.BootstrapConfig)
-
-	defaultReplicas := int32(0)
-	assert.Equal(t, &defaultReplicas, instance.Spec.ReplicantTemplate.Spec.Replicas)
-
-	assert.Equal(t, map[string]string{
-		"apps.emqx.io/managed-by": "emqx-operator",
-		"apps.emqx.io/instance":   "webhook-test",
-	}, instance.Labels)
-
-	assert.Equal(t, map[string]string{
-		"apps.emqx.io/managed-by": "emqx-operator",
-		"apps.emqx.io/instance":   "webhook-test",
-		"apps.emqx.io/db-role":    "core",
-	}, instance.Spec.CoreTemplate.Labels)
-
-	assert.Equal(t, map[string]string{
-		"apps.emqx.io/managed-by": "emqx-operator",
-		"apps.emqx.io/instance":   "webhook-test",
-		"apps.emqx.io/db-role":    "replicant",
-	}, instance.Spec.ReplicantTemplate.Labels)
 }
 
 func TestValidateCreate(t *testing.T) {
@@ -176,4 +67,155 @@ func TestValidateUpdate(t *testing.T) {
 
 	instance.Spec.BootstrapConfig = "foo = bar"
 	assert.Nil(t, instance.ValidateUpdate(old))
+}
+
+func TestValidateDelete(t *testing.T) {
+	instance := &EMQX{}
+	assert.Nil(t, instance.ValidateDelete())
+}
+
+func TestDefaultName(t *testing.T) {
+	instance := &EMQX{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "webhook-test",
+		},
+	}
+	instance.defaultNames()
+	assert.Equal(t, "webhook-test", instance.Name)
+	assert.Equal(t, "webhook-test-core", instance.Spec.CoreTemplate.Name)
+	assert.Equal(t, "webhook-test-replicant", instance.Spec.ReplicantTemplate.Name)
+	assert.Equal(t, "webhook-test-dashboard", instance.Spec.DashboardServiceTemplate.Name)
+	assert.Equal(t, "webhook-test-listeners", instance.Spec.ListenersServiceTemplate.Name)
+}
+
+func TestDefaultLabels(t *testing.T) {
+	instance := &EMQX{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "webhook-test",
+			Namespace: "default",
+		},
+	}
+	instance.defaultLabels()
+
+	assert.Equal(t, map[string]string{
+		"apps.emqx.io/managed-by": "emqx-operator",
+		"apps.emqx.io/instance":   "webhook-test",
+	}, instance.Labels)
+
+	assert.Equal(t, map[string]string{
+		"apps.emqx.io/managed-by": "emqx-operator",
+		"apps.emqx.io/instance":   "webhook-test",
+	}, instance.Spec.DashboardServiceTemplate.Labels)
+
+	assert.Equal(t, map[string]string{
+		"apps.emqx.io/managed-by": "emqx-operator",
+		"apps.emqx.io/instance":   "webhook-test",
+	}, instance.Spec.ListenersServiceTemplate.Labels)
+
+	assert.Equal(t, map[string]string{
+		"apps.emqx.io/managed-by": "emqx-operator",
+		"apps.emqx.io/instance":   "webhook-test",
+		"apps.emqx.io/db-role":    "core",
+	}, instance.Spec.CoreTemplate.Labels)
+
+	assert.Equal(t, map[string]string{
+		"apps.emqx.io/managed-by": "emqx-operator",
+		"apps.emqx.io/instance":   "webhook-test",
+		"apps.emqx.io/db-role":    "replicant",
+	}, instance.Spec.ReplicantTemplate.Labels)
+}
+
+func TestDefaultBootstrapConfig(t *testing.T) {
+	t.Run("empty bootstrap config", func(t *testing.T) {
+		instance := &EMQX{
+			Spec: EMQXSpec{
+				BootstrapConfig: "",
+			},
+		}
+		instance.defaultBootstrapConfig()
+
+		bootstrapConfig, err := hocon.ParseString(instance.Spec.BootstrapConfig)
+		assert.Nil(t, err)
+
+		assert.NotNil(t, bootstrapConfig.GetString("node.cookie"))
+		assert.Equal(t, "data", bootstrapConfig.GetString("node.data_dir"))
+		assert.Equal(t, "etc", bootstrapConfig.GetString("node.etc_dir"))
+
+		assert.Equal(t, "18083", bootstrapConfig.GetString("dashboard.listeners.http.bind"))
+		assert.Equal(t, "admin", bootstrapConfig.GetString("dashboard.default_username"))
+		assert.Equal(t, "public", bootstrapConfig.GetString("dashboard.default_password"))
+
+		assert.Equal(t, "\"0.0.0.0:1883\"", bootstrapConfig.GetString("listeners.tcp.default.bind"))
+		assert.Equal(t, "1024000", bootstrapConfig.GetString("listeners.tcp.default.max_connections"))
+	})
+
+	t.Run("already set cookie", func(t *testing.T) {
+		instance := &EMQX{
+			Spec: EMQXSpec{
+				BootstrapConfig: `node.cookie = "12345"`,
+			},
+		}
+		instance.defaultBootstrapConfig()
+
+		bootstrapConfig, err := hocon.ParseString(instance.Spec.BootstrapConfig)
+		assert.Nil(t, err)
+		assert.Equal(t, "12345", bootstrapConfig.GetString("node.cookie"))
+	})
+
+	t.Run("already set listener", func(t *testing.T) {
+		instance := &EMQX{
+			Spec: EMQXSpec{
+				BootstrapConfig: `listeners.tcp.default.bind = "0.0.0.0:11883"`,
+			},
+		}
+		instance.defaultBootstrapConfig()
+
+		bootstrapConfig, err := hocon.ParseString(instance.Spec.BootstrapConfig)
+		assert.Nil(t, err)
+		assert.Equal(t, "\"0.0.0.0:11883\"", bootstrapConfig.GetString("listeners.tcp.default.bind"))
+		assert.Equal(t, "1024000", bootstrapConfig.GetString("listeners.tcp.default.max_connections"))
+	})
+
+	t.Run("other style set listener", func(t *testing.T) {
+		instance := &EMQX{
+			Spec: EMQXSpec{
+				BootstrapConfig: `
+					listeners {
+						tcp {
+							default {
+								bind = "0.0.0.0:11883"
+							}
+						}
+					}
+					`,
+			},
+		}
+		instance.defaultBootstrapConfig()
+
+		bootstrapConfig, err := hocon.ParseString(instance.Spec.BootstrapConfig)
+		assert.Nil(t, err)
+		assert.Equal(t, "\"0.0.0.0:11883\"", bootstrapConfig.GetString("listeners.tcp.default.bind"))
+		assert.Equal(t, "1024000", bootstrapConfig.GetString("listeners.tcp.default.max_connections"))
+	})
+}
+
+func TestDefaultDashboardServiceTemplate(t *testing.T) {
+	instance := &EMQX{
+		Spec: EMQXSpec{
+			BootstrapConfig: `dashboard.listeners.http.bind = 18083`,
+			CoreTemplate: EMQXCoreTemplate{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"foo": "bar",
+					},
+				},
+			},
+		},
+	}
+	instance.defaultDashboardServiceTemplate()
+
+	assert.Equal(t, map[string]string{
+		"foo": "bar",
+	}, instance.Spec.DashboardServiceTemplate.Spec.Selector)
+	assert.Equal(t, int32(18083), instance.Spec.DashboardServiceTemplate.Spec.Ports[0].Port)
 }
