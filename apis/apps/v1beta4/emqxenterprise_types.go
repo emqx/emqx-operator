d71b69fde2acf12ea1f344b740d41a519b5b02fd
Rory Z <rory-z@outlook.com>
2022-10-13 10:02:35 +0800

    feat(v1beta4): add v1beta4 API Version

---
+++ apis/apps/v1beta4/emqxenterprise_types.go
@@ -0,0 +1,105 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta4
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type EmqxBlueGreenUpdateStrategy struct {
+	WaitTakeover  int32 `json:"waitTakeover,omitempty"`
+	ConnEvictRate int32 `json:"connEvictRate,omitempty"`
+	SessEvictRate int32 `json:"sessEvictRate,omitempty"`
+}
+
+// EmqxEnterpriseSpec defines the desired state of EmqxEnterprise
+type EmqxEnterpriseSpec struct {
+	//+kubebuilder:default:=3
+	Replicas *int32 `json:"replicas,omitempty"`
+
+	// VolumeClaimTemplates describes the common attributes of storage devices
+	VolumeClaimTemplates corev1.PersistentVolumeClaimSpec `json:"persistent,omitempty"`
+
+	EmqxBlueGreenUpdateStrategy EmqxBlueGreenUpdateStrategy `json:"blueGreenUpdateStrategy,omitempty"`
+
+	Template EmqxTemplate `json:"template,omitempty"`
+
+	// ServiceTemplate defines a logical set of ports and a policy by which to access them
+	ServiceTemplate ServiceTemplate `json:"serviceTemplate,omitempty"`
+}
+
+// EmqxEnterpriseStatus defines the observed state of EmqxEnterprise
+type EmqxEnterpriseStatus struct {
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+//+kubebuilder:subresource:scale:specpath=.spec.replicas,statuspath=.status.replicas
+
+// EmqxEnterprise is the Schema for the emqxenterprises API
+type EmqxEnterprise struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   EmqxEnterpriseSpec `json:"spec,omitempty"`
+	Status `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// EmqxEnterpriseList contains a list of EmqxEnterprise
+type EmqxEnterpriseList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []EmqxEnterprise `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&EmqxEnterprise{}, &EmqxEnterpriseList{})
+}
+
+func (emqx *EmqxEnterprise) GetReplicas() *int32 {
+	return emqx.Spec.Replicas
+}
+
+func (emqx *EmqxEnterprise) SetReplicas(replicas int32) {
+	emqx.Spec.Replicas = &replicas
+}
+
+func (emqx *EmqxEnterprise) GetVolumeClaimTemplates() corev1.PersistentVolumeClaimSpec {
+	return emqx.Spec.VolumeClaimTemplates
+}
+
+func (emqx *EmqxEnterprise) SetVolumeClaimTemplates(volumeClaimTemplates corev1.PersistentVolumeClaimSpec) {
+	emqx.Spec.VolumeClaimTemplates = volumeClaimTemplates
+}
+
+func (emqx *EmqxEnterprise) GetTemplate() EmqxTemplate {
+	return emqx.Spec.Template
+}
+
+func (emqx *EmqxEnterprise) SetTemplate(template EmqxTemplate) {
+	emqx.Spec.Template = template
+}
+
+func (emqx *EmqxEnterprise) GetServiceTemplate() ServiceTemplate {
+	return emqx.Spec.ServiceTemplate
+}
+func (emqx *EmqxEnterprise) SetServiceTemplate(serviceTemplate ServiceTemplate) {
+	emqx.Spec.ServiceTemplate = serviceTemplate
+}
---
+++ apis/apps/v1beta4/status_types.go
@@ -0,0 +1,143 @@
+package v1beta4
+
+import (
+	"sort"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Phase of the RF status
+type Phase string
+
+// Condition saves the state information of the EMQX cluster
+type Condition struct {
+	// Status of cluster condition.
+	Type ConditionType `json:"type"`
+	// Status of the condition, one of True, False, Unknown.
+	Status corev1.ConditionStatus `json:"status"`
+	// The last time this condition was updated.
+	LastUpdateTime string      `json:"lastUpdateTime,omitempty"`
+	LastUpdateAt   metav1.Time `json:"-"`
+	// Last time the condition transitioned from one status to another.
+	LastTransitionTime string `json:"lastTransitionTime,omitempty"`
+	// The reason for the condition's last transition.
+	Reason string `json:"reason,omitempty"`
+	// A human readable message indicating details about the transition.
+	Message string `json:"message,omitempty"`
+}
+
+// ConditionType defines the condition that the RF can have
+type ConditionType string
+
+const (
+	ConditionPluginInitialized ConditionType = "PluginInitialized"
+	ConditionRunning           ConditionType = "Running"
+)
+
+//+kubebuilder:object:generate=false
+type EmqxStatus interface {
+	IsRunning() bool
+	IsPluginInitialized() bool
+	GetConditions() []Condition
+	SetCondition(c Condition)
+}
+
+type EmqxNode struct {
+	// EMQX node name
+	Node string `json:"node,omitempty"`
+	// EMQX node status
+	NodeStatus string `json:"node_status,omitempty"`
+	// Erlang/OTP version used by EMQX
+	OTPRelease string `json:"otp_release,omitempty"`
+	// EMQX version
+	Version string `json:"version,omitempty"`
+}
+
+// Emqx Status defines the observed state of EMQX
+type Status struct {
+	// Represents the latest available observations of a EMQX current state.
+	Conditions []Condition `json:"conditions,omitempty"`
+	// Nodes of the EMQX cluster
+	EmqxNodes []EmqxNode `json:"emqxNodes,omitempty"`
+	// replicas is the number of Pods created by the EMQX Custom Resource controller.
+	Replicas int32 `json:"replicas,omitempty"`
+	// readyReplicas is the number of pods created for this EMQX Custom Resource with a EMQX Ready.
+	ReadyReplicas int32 `json:"readyReplicas,omitempty"`
+}
+
+func NewCondition(condType ConditionType, status corev1.ConditionStatus, reason, message string) *Condition {
+	now := metav1.Now()
+	nowString := now.Format(time.RFC3339)
+	return &Condition{
+		Type:               condType,
+		Status:             status,
+		LastUpdateTime:     nowString,
+		LastUpdateAt:       now,
+		LastTransitionTime: nowString,
+		Reason:             reason,
+		Message:            message,
+	}
+}
+
+func (s *Status) IsRunning() bool {
+	if len(s.Conditions) == 0 {
+		return false
+	}
+	c := s.Conditions[0]
+	if c.Type == ConditionRunning && c.Status == corev1.ConditionTrue {
+		return true
+	}
+	return false
+}
+
+func (s *Status) IsPluginInitialized() bool {
+	// Init Plugin
+	for _, c := range s.Conditions {
+		if c.Type == ConditionPluginInitialized && c.Status == corev1.ConditionTrue {
+			return true
+		}
+	}
+	return false
+}
+
+func (s *Status) GetConditions() []Condition {
+	return s.Conditions
+}
+
+func (s *Status) SetCondition(c Condition) {
+	pos, cp := getCondition(s, c.Type)
+	if cp != nil &&
+		cp.Status == c.Status && cp.Reason == c.Reason && cp.Message == c.Message {
+		now := metav1.Now()
+		nowString := now.Format(time.RFC3339)
+		s.Conditions[pos].LastUpdateAt = now
+		s.Conditions[pos].LastUpdateTime = nowString
+		s.sortConditions(s.Conditions)
+		return
+	}
+
+	if cp != nil {
+		s.Conditions[pos] = c
+	} else {
+		s.Conditions = append(s.Conditions, c)
+	}
+
+	s.sortConditions(s.Conditions)
+}
+
+func (s *Status) sortConditions(conditions []Condition) {
+	sort.Slice(conditions, func(i, j int) bool {
+		return s.Conditions[j].LastUpdateAt.Before(&s.Conditions[i].LastUpdateAt)
+	})
+}
+
+func getCondition(status *Status, t ConditionType) (int, *Condition) {
+	for i, c := range status.Conditions {
+		if t == c.Type {
+			return i, &c
+		}
+	}
+	return -1, nil
+}
