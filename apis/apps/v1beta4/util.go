3c47077ea6afceae5ed36ab2b786c9a28e1ee364
Rory Z <rory-z@outlook.com>
2022-10-13 10:02:35 +0800

    feat(v1beta4): add v1beta4 API Version

---
+++ apis/apps/v1beta4/util.go
@@ -0,0 +1,49 @@
+package v1beta4
+
+import (
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func (s *ServiceTemplate) MergePorts(ports []corev1.ServicePort) {
+	ports = append(s.Spec.Ports, ports...)
+
+	result := make([]corev1.ServicePort, 0, len(ports))
+	temp := map[string]struct{}{}
+
+	for _, item := range ports {
+		if _, ok := temp[item.Name]; !ok {
+			temp[item.Name] = struct{}{}
+			result = append(result, item)
+		}
+	}
+
+	s.Spec.Ports = result
+}
+
+//+kubebuilder:object:generate=false
+type Names struct {
+	metav1.Object
+}
+
+func (n Names) HeadlessSvc() string {
+	return fmt.Sprintf("%s-%s", n.Object.GetName(), "headless")
+}
+
+func (n Names) License() string {
+	return fmt.Sprintf("%s-%s", n.Object.GetName(), "license")
+}
+
+func (n Names) ACL() string {
+	return fmt.Sprintf("%s-%s", n.Object.GetName(), "acl")
+}
+
+func (n Names) PluginsConfig() string {
+	return fmt.Sprintf("%s-%s", n.Object.GetName(), "plugins-config")
+}
+
+func (n Names) Data() string {
+	return fmt.Sprintf("%s-%s", n.Object.GetName(), "data")
+}
---
+++ apis/apps/v1beta4/util_test.go
@@ -0,0 +1,43 @@
+package v1beta4
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+)
+
+func TestMergePorts(t *testing.T) {
+	serviceTemplate := &ServiceTemplate{
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{
+					Name: "exist",
+					Port: 8080,
+				},
+			},
+		},
+	}
+
+	serviceTemplate.MergePorts([]corev1.ServicePort{
+		{
+			Name: "exist",
+			Port: 8081,
+		},
+		{
+			Name: "not-exist",
+			Port: 8082,
+		},
+	})
+
+	assert.ElementsMatch(t, serviceTemplate.Spec.Ports, []corev1.ServicePort{
+		{
+			Name: "exist",
+			Port: 8080,
+		},
+		{
+			Name: "not-exist",
+			Port: 8082,
+		},
+	})
+}
