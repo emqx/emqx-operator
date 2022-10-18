44ebaf4f00decb764012f90db9ea955956cd97c2
Rory Z <rory-z@outlook.com>
2022-10-18 17:21:05 +0800

    test(v1beta3): refactor e2e test case

--- e2e/v1beta3/e2e_test.go
+++ e2e/v1beta3/e2e_test.go
@@ -26,25 +26,112 @@ import (
 	. "github.com/onsi/gomega"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
-var _ = Describe("Check EMQX Custom Resource", func() {
-	var headlessPort corev1.ServicePort
-	var ports, pluginPorts []corev1.ServicePort
-	var pluginList []string
+var emqxBroker = &appsv1beta3.EmqxBroker{
+	ObjectMeta: metav1.ObjectMeta{
+		Name:      "emqx",
+		Namespace: "default",
+		Labels: map[string]string{
+			"test": "e2e",
+		},
+	},
+	Spec: appsv1beta3.EmqxBrokerSpec{
+		EmqxTemplate: appsv1beta3.EmqxBrokerTemplate{
+			Image: "emqx/emqx:4.4.8",
+		},
+	},
+}
 
-	Context("check EMQX Custom Resources", func() {
-		BeforeEach(func() {
-			headlessPort = corev1.ServicePort{
-				Name:       "http-management-8081",
-				Port:       8081,
-				Protocol:   corev1.ProtocolTCP,
-				TargetPort: intstr.FromInt(8081),
+var emqxEnterprise = &appsv1beta3.EmqxEnterprise{
+	ObjectMeta: metav1.ObjectMeta{
+		Name:      "emqx-ee",
+		Namespace: "default",
+		Labels: map[string]string{
+			"test": "e2e",
+		},
+	},
+	Spec: appsv1beta3.EmqxEnterpriseSpec{
+		EmqxTemplate: appsv1beta3.EmqxEnterpriseTemplate{
+			Image: "emqx/emqx:4.4.8",
+		},
+	},
+}
+
+var lwm2m = &appsv1beta3.EmqxPlugin{
+	TypeMeta: metav1.TypeMeta{
+		APIVersion: "apps.emqx.io/v1beta3",
+		Kind:       "EmqxPlugin",
+	},
+	ObjectMeta: metav1.ObjectMeta{
+		Name:      "lwm2m",
+		Namespace: "default",
+		Labels: map[string]string{
+			"test": "e2e",
+		},
+	},
+	Spec: appsv1beta3.EmqxPluginSpec{
+		PluginName: "emqx_lwm2m",
+		Selector: map[string]string{
+			"test": "e2e",
+		},
+		Config: map[string]string{
+			"lwm2m.lifetime_min": "1s",
+			"lwm2m.lifetime_max": "86400s",
+			"lwm2m.bind.udp.1":   "0.0.0.0:5685",
+			"lwm2m.bind.udp.2":   "0.0.0.0:5686",
+			"lwm2m.bind.dtls.1":  "0.0.0.0:5687",
+			"lwm2m.bind.dtls.2":  "0.0.0.0:5688",
+			"lwm2m.xml_dir":      "/opt/emqx/etc/lwm2m_xml",
+		},
+	},
+}
+
+var _ = Describe("", func() {
+	DescribeTable("",
+		func(emqx appsv1beta3.Emqx, plugin *appsv1beta3.EmqxPlugin) {
+			var pluginList []string
+			var pluginPorts []corev1.ServicePort
+			var ports []corev1.ServicePort
+			var headlessPort corev1.ServicePort
+
+			pluginList = []string{"emqx_rule_engine", "emqx_retainer", "emqx_lwm2m"}
+			if _, ok := emqx.(*appsv1beta3.EmqxEnterprise); ok {
+				pluginList = append(pluginList, "emqx_modules")
 			}
+
+			pluginPorts = []corev1.ServicePort{
+				{
+					Name:       "lwm2m-udp-5685",
+					Protocol:   corev1.ProtocolUDP,
+					Port:       5685,
+					TargetPort: intstr.FromInt(5685),
+				},
+				{
+					Name:       "lwm2m-udp-5686",
+					Protocol:   corev1.ProtocolUDP,
+					Port:       5686,
+					TargetPort: intstr.FromInt(5686),
+				},
+				{
+					Name:       "lwm2m-dtls-5687",
+					Protocol:   corev1.ProtocolUDP,
+					Port:       5687,
+					TargetPort: intstr.FromInt(5687),
+				},
+				{
+					Name:       "lwm2m-dtls-5688",
+					Protocol:   corev1.ProtocolUDP,
+					Port:       5688,
+					TargetPort: intstr.FromInt(5688),
+				},
+			}
+
 			ports = []corev1.ServicePort{
 				{
 					Name:       "mqtt-tcp-1883",
@@ -71,248 +158,197 @@ var _ = Describe("Check EMQX Custom Resource", func() {
 					TargetPort: intstr.FromInt(8084),
 				},
 			}
-			pluginPorts = []corev1.ServicePort{
-				{
-					Name:       "lwm2m-udp-5683",
-					Protocol:   corev1.ProtocolUDP,
-					Port:       5683,
-					TargetPort: intstr.FromInt(5683),
-				},
-				{
-					Name:       "lwm2m-udp-5684",
-					Protocol:   corev1.ProtocolUDP,
-					Port:       5684,
-					TargetPort: intstr.FromInt(5684),
-				},
-				{
-					Name:       "lwm2m-dtls-5685",
-					Protocol:   corev1.ProtocolUDP,
-					Port:       5685,
-					TargetPort: intstr.FromInt(5685),
-				},
-				{
-					Name:       "lwm2m-dtls-5686",
-					Protocol:   corev1.ProtocolUDP,
-					Port:       5686,
-					TargetPort: intstr.FromInt(5686),
-				},
+
+			headlessPort = corev1.ServicePort{
+				Name:       "http-management-8081",
+				Port:       8081,
+				Protocol:   corev1.ProtocolTCP,
+				TargetPort: intstr.FromInt(8081),
 			}
-			pluginList = []string{"emqx_rule_engine", "emqx_retainer", "emqx_lwm2m"}
-		})
-
-		It("check default resource", func() {
-			By("should check EMQX CR Status")
-			check_emqx_status(broker)
-			check_emqx_status(enterprise)
-
-			By("should check StatefulSet annotations")
-			check_pod_annotations(broker)
-			check_pod_annotations(enterprise)
-
-			By("should create EMQX Plugins")
-			check_plugin(broker, pluginList)
-			check_plugin(enterprise, append(pluginList, "emqx_modules"))
-
-			By("should bind listener ports to service")
-			check_service_ports(broker, append(ports, pluginPorts...), headlessPort)
-			check_service_ports(enterprise, append(ports, pluginPorts...), headlessPort)
-
-		})
-	})
-
-	Context("check update EMQX Plugins", func() {
-		BeforeEach(func() {
-			pluginPorts = []corev1.ServicePort{
-				{
-					Name:       "lwm2m-dtls-5685",
-					Protocol:   corev1.ProtocolUDP,
-					Port:       5685,
-					TargetPort: intstr.FromInt(5685),
-				},
-				{
-					Name:       "lwm2m-dtls-5686",
-					Protocol:   corev1.ProtocolUDP,
-					Port:       5686,
-					TargetPort: intstr.FromInt(5686),
-				},
-				{
-					Name:       "lwm2m-udp-5687",
-					Protocol:   corev1.ProtocolUDP,
-					Port:       5687,
-					TargetPort: intstr.FromInt(5687),
-				},
-				{
-					Name:       "lwm2m-udp-5688",
-					Protocol:   corev1.ProtocolUDP,
-					Port:       5688,
-					TargetPort: intstr.FromInt(5688),
-				},
-			}
-
-			update_lwm2m(broker)
-			update_lwm2m(enterprise)
-		})
-		It("", func() {
-			By("should bind ports to service")
-			check_service_ports(broker, append(ports, pluginPorts...), headlessPort)
-			check_service_ports(enterprise, append(ports, pluginPorts...), headlessPort)
-		})
-
-		AfterEach(func() {
-			Eventually(func() error {
-				plugin := &appsv1beta3.EmqxPlugin{}
-				err := k8sClient.Get(
-					context.Background(),
+			By("create EMQX CR")
+			emqx.Default()
+			Expect(emqx.ValidateCreate()).Should(Succeed())
+			Expect(k8sClient.Create(context.TODO(), emqx)).Should(Succeed())
+			Eventually(func() bool {
+				_ = k8sClient.Get(
+					context.TODO(),
 					types.NamespacedName{
-						Name:      fmt.Sprintf("%s-%s", broker.GetName(), "lwm2m"),
-						Namespace: broker.GetNamespace(),
-					}, plugin,
+						Name:      emqx.GetName(),
+						Namespace: emqx.GetNamespace(),
+					},
+					emqx,
 				)
-				if err != nil {
-					if k8sErrors.IsNotFound(err) {
-						return nil
-					}
-					return err
+				return emqx.IsRunning()
+			}, timeout, interval).Should(BeTrue())
+
+			By("create EMQX Plugin")
+			plugin.Labels = emqx.GetLabels()
+			Expect(k8sClient.Create(context.TODO(), plugin)).Should(Succeed())
+
+			By("check EMQX CR status")
+			Expect(emqx.GetStatus().Replicas).Should(Equal(int32(3)))
+			Expect(emqx.GetStatus().ReadyReplicas).Should(Equal(int32(3)))
+			Expect(emqx.GetStatus().EmqxNodes).Should(HaveLen(3))
+
+			By("check pod annotations")
+			sts := &appsv1.StatefulSet{}
+			Expect(k8sClient.Get(context.TODO(), client.ObjectKeyFromObject(emqx), sts)).Should(Succeed())
+			Expect(sts.Spec.Template.Annotations).Should(HaveKey(handler.ManageContainersAnnotation))
+			if len(emqx.GetACL()) != 0 {
+				Expect(sts.Spec.Template.Annotations).Should(HaveKey("ACL/Base64EncodeConfig"))
+			} else {
+				Expect(sts.Spec.Template.Annotations).ShouldNot(HaveKey("ACL/Base64EncodeConfig"))
+			}
+			switch instance := emqx.(type) {
+			case *appsv1beta3.EmqxBroker:
+				if len(instance.GetModules()) != 0 {
+					Expect(sts.Spec.Template.Annotations).Should(HaveKey("LoadedModules/Base64EncodeConfig"))
+				} else {
+					Expect(sts.Spec.Template.Annotations).ShouldNot(HaveKey("LoadedModules/Base64EncodeConfig"))
 				}
-				return k8sClient.Delete(context.Background(), plugin)
-			}, timeout, interval).Should(Succeed())
+			case *appsv1beta3.EmqxEnterprise:
+				if len(instance.GetModules()) != 0 {
+					Expect(sts.Spec.Template.Annotations).Should(HaveKey("LoadedModules/Base64EncodeConfig"))
+				} else {
+					Expect(sts.Spec.Template.Annotations).ShouldNot(HaveKey("LoadedModules/Base64EncodeConfig"))
+				}
+			}
 
-			By("should delete plugin resource")
+			By("check plugins")
+			Eventually(func() []string {
+				list := appsv1beta3.EmqxPluginList{}
+				_ = k8sClient.List(
+					context.Background(),
+					&list,
+					client.InNamespace(emqx.GetNamespace()),
+					client.MatchingLabels(emqx.GetLabels()),
+				)
+				l := []string{}
+				for _, plugin := range list.Items {
+					l = append(l, plugin.Spec.PluginName)
+				}
+				return l
+			}, timeout, interval).Should(ConsistOf(pluginList))
 
+			for _, pluginName := range pluginList {
+				Eventually(func() map[string]string {
+					cm := &corev1.ConfigMap{}
+					_ = k8sClient.Get(
+						context.Background(),
+						types.NamespacedName{
+							Name:      fmt.Sprintf("%s-%s", emqx.GetName(), "plugins-config"),
+							Namespace: emqx.GetNamespace(),
+						}, cm,
+					)
+					return cm.Data
+				}, timeout, interval).Should(HaveKey(pluginName + ".conf"))
+			}
+
+			By("check headless service ports")
 			Eventually(func() []corev1.ServicePort {
 				svc := &corev1.Service{}
 				_ = k8sClient.Get(
 					context.Background(),
 					types.NamespacedName{
-						Name:      broker.GetName(),
-						Namespace: broker.GetNamespace(),
+						Name:      fmt.Sprintf("%s-%s", emqx.GetName(), "headless"),
+						Namespace: emqx.GetNamespace(),
 					},
 					svc,
 				)
 				return svc.Spec.Ports
-			}, timeout, interval).ShouldNot(ContainElements(pluginPorts))
-		})
-	})
-})
+			}, timeout, interval).Should(ContainElements(headlessPort))
 
-func update_lwm2m(emqx appsv1beta3.Emqx) {
-	Eventually(func() error {
-		plugin := &appsv1beta3.EmqxPlugin{}
-		err := k8sClient.Get(
-			context.Background(),
-			types.NamespacedName{
-				Name:      fmt.Sprintf("%s-%s", emqx.GetName(), "lwm2m"),
-				Namespace: emqx.GetNamespace(),
-			}, plugin,
-		)
-		if err != nil {
-			return err
-		}
-		plugin.Spec.Config["lwm2m.bind.udp.1"] = "0.0.0.0:5687"
-		plugin.Spec.Config["lwm2m.bind.udp.2"] = "0.0.0.0:5688"
-		return k8sClient.Update(context.Background(), plugin)
-	}, timeout, interval).Should(Succeed())
-}
+			By("check service ports")
+			Eventually(func() []corev1.ServicePort {
+				svc := &corev1.Service{}
+				_ = k8sClient.Get(
+					context.Background(),
+					types.NamespacedName{
+						Name:      emqx.GetName(),
+						Namespace: emqx.GetNamespace(),
+					},
+					svc,
+				)
+				return svc.Spec.Ports
+			}, timeout, interval).Should(ContainElements(append(pluginPorts, append(ports, headlessPort)...)))
 
-func check_emqx_status(emqx appsv1beta3.Emqx) {
-	Eventually(func() bool {
-		_ = k8sClient.Get(
-			context.TODO(),
-			types.NamespacedName{
-				Name:      emqx.GetName(),
-				Namespace: emqx.GetNamespace(),
-			},
-			emqx,
-		)
-		status := emqx.GetStatus()
-		return status.IsRunning()
-	}, timeout, interval).Should(BeTrue())
+			By("update EMQX Plugin")
+			Eventually(func() error {
+				plugin := &appsv1beta3.EmqxPlugin{}
+				err := k8sClient.Get(
+					context.Background(),
+					types.NamespacedName{
+						Name:      lwm2m.GetName(),
+						Namespace: lwm2m.GetNamespace(),
+					}, plugin,
+				)
+				if err != nil {
+					return err
+				}
+				plugin.Spec.Config["lwm2m.bind.udp.1"] = "0.0.0.0:5695"
+				plugin.Spec.Config["lwm2m.bind.udp.2"] = "0.0.0.0:5696"
+				plugin.Spec.Config["lwm2m.bind.dtls.1"] = "0.0.0.0:5697"
+				plugin.Spec.Config["lwm2m.bind.dtls.2"] = "0.0.0.0:5698"
+				return k8sClient.Update(context.Background(), plugin)
+			}, timeout, interval).Should(Succeed())
 
-	Expect(emqx.GetStatus().Replicas).Should(Equal(int32(3)))
-	Expect(emqx.GetStatus().ReadyReplicas).Should(Equal(int32(3)))
-	Expect(emqx.GetStatus().EmqxNodes).Should(HaveLen(3))
-}
+			pluginPorts = []corev1.ServicePort{
+				{
+					Name:       "lwm2m-udp-5695",
+					Protocol:   corev1.ProtocolUDP,
+					Port:       5695,
+					TargetPort: intstr.FromInt(5695),
+				},
+				{
+					Name:       "lwm2m-udp-5696",
+					Protocol:   corev1.ProtocolUDP,
+					Port:       5696,
+					TargetPort: intstr.FromInt(5696),
+				},
+				{
+					Name:       "lwm2m-dtls-5697",
+					Protocol:   corev1.ProtocolUDP,
+					Port:       5697,
+					TargetPort: intstr.FromInt(5697),
+				},
+				{
+					Name:       "lwm2m-dtls-5698",
+					Protocol:   corev1.ProtocolUDP,
+					Port:       5698,
+					TargetPort: intstr.FromInt(5698),
+				},
+			}
 
-func check_pod_annotations(emqx appsv1beta3.Emqx) {
-	sts := &appsv1.StatefulSet{}
-	Expect(k8sClient.Get(context.TODO(), client.ObjectKeyFromObject(emqx), sts)).Should(Succeed())
-	Expect(sts.Spec.Template.Annotations).Should(HaveKey(handler.ManageContainersAnnotation))
-	if len(emqx.GetACL()) != 0 {
-		Expect(sts.Spec.Template.Annotations).Should(HaveKey("ACL/Base64EncodeConfig"))
-	} else {
-		Expect(sts.Spec.Template.Annotations).ShouldNot(HaveKey("ACL/Base64EncodeConfig"))
-	}
+			By("check service ports")
+			Eventually(func() []corev1.ServicePort {
+				svc := &corev1.Service{}
+				_ = k8sClient.Get(
+					context.Background(),
+					types.NamespacedName{
+						Name:      emqx.GetName(),
+						Namespace: emqx.GetNamespace(),
+					},
+					svc,
+				)
+				return svc.Spec.Ports
+			}, timeout, interval).Should(ContainElements(append(pluginPorts, append(ports, headlessPort)...)))
 
-	switch instance := emqx.(type) {
-	case *appsv1beta3.EmqxBroker:
-		if len(instance.GetModules()) != 0 {
-			Expect(sts.Spec.Template.Annotations).Should(HaveKey("LoadedModules/Base64EncodeConfig"))
-		} else {
-			Expect(sts.Spec.Template.Annotations).ShouldNot(HaveKey("LoadedModules/Base64EncodeConfig"))
-		}
-	case *appsv1beta3.EmqxEnterprise:
-		if len(instance.GetModules()) != 0 {
-			Expect(sts.Spec.Template.Annotations).Should(HaveKey("LoadedModules/Base64EncodeConfig"))
-		} else {
-			Expect(sts.Spec.Template.Annotations).ShouldNot(HaveKey("LoadedModules/Base64EncodeConfig"))
-		}
-	}
-}
-
-func check_service_ports(emqx appsv1beta3.Emqx, ports []corev1.ServicePort, headlessPort corev1.ServicePort) {
-	Eventually(func() []corev1.ServicePort {
-		svc := &corev1.Service{}
-		_ = k8sClient.Get(
-			context.Background(),
-			types.NamespacedName{
-				Name:      fmt.Sprintf("%s-%s", emqx.GetName(), "headless"),
-				Namespace: emqx.GetNamespace(),
-			},
-			svc,
-		)
-		return svc.Spec.Ports
-	}, timeout, interval).Should(ContainElements(headlessPort))
-	Eventually(func() []corev1.ServicePort {
-		svc := &corev1.Service{}
-		_ = k8sClient.Get(
-			context.Background(),
-			types.NamespacedName{
-				Name:      emqx.GetName(),
-				Namespace: emqx.GetNamespace(),
-			},
-			svc,
-		)
-		return svc.Spec.Ports
-	}, timeout, interval).Should(ContainElements(append(ports, headlessPort)))
-}
-
-func check_plugin(emqx appsv1beta3.Emqx, pluginList []string) {
-	Eventually(func() []string {
-		list := appsv1beta3.EmqxPluginList{}
-		_ = k8sClient.List(
-			context.Background(),
-			&list,
-			client.InNamespace(emqx.GetNamespace()),
-			client.MatchingLabels(emqx.GetLabels()),
-		)
-		l := []string{}
-		for _, plugin := range list.Items {
-			l = append(l, plugin.Spec.PluginName)
-		}
-		return l
-	}, timeout, interval).Should(ConsistOf(pluginList))
-
-	for _, pluginName := range pluginList {
-		Eventually(func() map[string]string {
-			cm := &corev1.ConfigMap{}
-			_ = k8sClient.Get(
+			By("delete EMQX CR and EMQX Plugin")
+			finalizer := "apps.emqx.io/finalizer"
+			plugins := &appsv1beta3.EmqxPluginList{}
+			_ = k8sClient.List(
 				context.Background(),
-				types.NamespacedName{
-					Name:      fmt.Sprintf("%s-%s", emqx.GetName(), "plugins-config"),
-					Namespace: emqx.GetNamespace(),
-				}, cm,
+				plugins,
+				client.InNamespace("default"),
 			)
-			return cm.Data
-		}, timeout, interval).Should(HaveKey(pluginName + ".conf"))
-	}
-}
+			for _, plugin := range plugins.Items {
+				controllerutil.RemoveFinalizer(&plugin, finalizer)
+				Expect(k8sClient.Update(context.Background(), &plugin)).Should(Succeed())
+				Expect(k8sClient.Delete(context.Background(), &plugin)).Should(Succeed())
+			}
+			Expect(k8sClient.Delete(context.TODO(), emqx)).Should(Succeed())
+		},
+		Entry(nil, emqxBroker.DeepCopy(), lwm2m.DeepCopy()),
+		Entry(nil, emqxEnterprise.DeepCopy(), lwm2m.DeepCopy()),
+	)
+})
--- e2e/v1beta3/suite_test.go
+++ e2e/v1beta3/suite_test.go
@@ -16,8 +16,6 @@ limitations under the License.
 package v1beta3
 
 import (
-	"context"
-	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
@@ -29,17 +27,10 @@ import (
 	appsv1beta3 "github.com/emqx/emqx-operator/apis/apps/v1beta3"
 	appscontrollersv1beta3 "github.com/emqx/emqx-operator/controllers/apps/v1beta3"
 	"github.com/emqx/emqx-operator/pkg/handler"
-	corev1 "k8s.io/api/core/v1"
-	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/api/resource"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
@@ -50,11 +41,6 @@ var timeout, interval time.Duration
 var k8sClient client.Client
 var clientset *kubernetes.Clientset
 var testEnv *envtest.Environment
-
-var storageClassName string = "standard"
-
-var broker *appsv1beta3.EmqxBroker = new(appsv1beta3.EmqxBroker)
-var enterprise *appsv1beta3.EmqxEnterprise = new(appsv1beta3.EmqxEnterprise)
 
 func TestSuites(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -142,192 +128,9 @@ var _ = BeforeSuite(func() {
 		err = k8sManager.Start(ctrl.SetupSignalHandler())
 		Expect(err).ToNot(HaveOccurred(), "failed to run manager")
 	}()
-
-	broker = &appsv1beta3.EmqxBroker{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "emqx",
-			Namespace: "broker",
-			Labels: map[string]string{
-				"cluster": "emqx",
-			},
-		},
-		Spec: appsv1beta3.EmqxBrokerSpec{
-			EmqxTemplate: appsv1beta3.EmqxBrokerTemplate{
-				Image: "emqx/emqx:4.4.8",
-			},
-		},
-	}
-	broker.Default()
-
-	enterprise = &appsv1beta3.EmqxEnterprise{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "emqx-ee",
-			Namespace: "enterprise",
-			Labels: map[string]string{
-				"cluster": "emqx",
-			},
-		},
-		Spec: appsv1beta3.EmqxEnterpriseSpec{
-			Persistent: corev1.PersistentVolumeClaimSpec{
-				StorageClassName: &storageClassName,
-				AccessModes:      []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce},
-				Resources: corev1.ResourceRequirements{
-					Requests: corev1.ResourceList{
-						corev1.ResourceStorage: resource.MustParse("20Mi"),
-					},
-				},
-			},
-			EmqxTemplate: appsv1beta3.EmqxEnterpriseTemplate{
-				Image: "emqx/emqx-ee:4.4.8",
-				ACL: []string{
-					"{allow, all}",
-				},
-				Modules: []appsv1beta3.EmqxEnterpriseModule{
-					{
-						Name:    "internal_acl",
-						Enable:  true,
-						Configs: runtime.RawExtension{Raw: []byte(`{"acl_rule_file": "/mounted/acl/acl.conf"}`)},
-					},
-				},
-				License: appsv1beta3.License{
-					Data: []byte(`-----BEGIN CERTIFICATE-----
-MIIENzCCAx+gAwIBAgIDdMvVMA0GCSqGSIb3DQEBBQUAMIGDMQswCQYDVQQGEwJD
-TjERMA8GA1UECAwIWmhlamlhbmcxETAPBgNVBAcMCEhhbmd6aG91MQwwCgYDVQQK
-DANFTVExDDAKBgNVBAsMA0VNUTESMBAGA1UEAwwJKi5lbXF4LmlvMR4wHAYJKoZI
-hvcNAQkBFg96aGFuZ3doQGVtcXguaW8wHhcNMjAwNjIwMDMwMjUyWhcNNDkwMTAx
-MDMwMjUyWjBjMQswCQYDVQQGEwJDTjEZMBcGA1UECgwQRU1RIFggRXZhbHVhdGlv
-bjEZMBcGA1UEAwwQRU1RIFggRXZhbHVhdGlvbjEeMBwGCSqGSIb3DQEJARYPY29u
-dGFjdEBlbXF4LmlvMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArw+3
-2w9B7Rr3M7IOiMc7OD3Nzv2KUwtK6OSQ07Y7ikDJh0jynWcw6QamTiRWM2Ale8jr
-0XAmKgwUSI42+f4w84nPpAH4k1L0zupaR10VYKIowZqXVEvSyV8G2N7091+6Jcon
-DcaNBqZLRe1DiZXMJlhXnDgq14FPAxffKhCXiCgYtluLDDLKv+w9BaQGZVjxlFe5
-cw32+z/xHU366npHBpafCbxBtWsNvchMVtLBqv9yPmrMqeBROyoJaI3nL78xDgpd
-cRorqo+uQ1HWdcM6InEFET6pwkeuAF8/jJRlT12XGgZKKgFQTCkZi4hv7aywkGBE
-JruPif/wlK0YuPJu6QIDAQABo4HSMIHPMBEGCSsGAQQBg5odAQQEDAIxMDCBlAYJ
-KwYBBAGDmh0CBIGGDIGDZW1xeF9iYWNrZW5kX3JlZGlzLGVtcXhfYmFja2VuZF9t
-eXNxbCxlbXF4X2JhY2tlbmRfcGdzcWwsZW1xeF9iYWNrZW5kX21vbmdvLGVtcXhf
-YmFja2VuZF9jYXNzYSxlbXF4X2JyaWRnZV9rYWZrYSxlbXF4X2JyaWRnZV9yYWJi
-aXQwEAYJKwYBBAGDmh0DBAMMATEwEQYJKwYBBAGDmh0EBAQMAjEwMA0GCSqGSIb3
-DQEBBQUAA4IBAQDHUe6+P2U4jMD23u96vxCeQrhc/rXWvpmU5XB8Q/VGnJTmv3yU
-EPyTFKtEZYVX29z16xoipUE6crlHhETOfezYsm9K0DxF3fNilOLRKkg9VEWcb5hj
-iL3a2tdZ4sq+h/Z1elIXD71JJBAImjr6BljTIdUCfVtNvxlE8M0D/rKSn2jwzsjI
-UrW88THMtlz9sb56kmM3JIOoIJoep6xNEajIBnoChSGjtBYFNFwzdwSTCodYkgPu
-JifqxTKSuwAGSlqxJUwhjWG8ulzL3/pCAYEwlWmd2+nsfotQdiANdaPnez7o0z0s
-EujOCZMbK8qNfSbyo50q5iIXhz2ZIGl+4hdp
------END CERTIFICATE-----`),
-				},
-			},
-		},
-	}
-	enterprise.Default()
-	emqxReady := make(chan string)
-	for _, emqx := range []appsv1beta3.Emqx{broker, enterprise} {
-		go func(emqx appsv1beta3.Emqx) {
-			Expect(k8sClient.Create(context.Background(), &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: emqx.GetNamespace()}})).Should(Succeed())
-			Expect(k8sClient.Create(context.Background(), emqx)).Should(Succeed())
-
-			Eventually(func() bool {
-				var instance appsv1beta3.Emqx
-				switch emqx.(type) {
-				case *appsv1beta3.EmqxBroker:
-					instance = &appsv1beta3.EmqxBroker{}
-				case *appsv1beta3.EmqxEnterprise:
-					instance = &appsv1beta3.EmqxEnterprise{}
-				}
-				_ = k8sClient.Get(
-					context.TODO(),
-					types.NamespacedName{
-						Name:      emqx.GetName(),
-						Namespace: emqx.GetNamespace(),
-					},
-					instance,
-				)
-				status := instance.GetStatus()
-				return status.IsRunning()
-			}, timeout, interval).Should(BeTrue())
-
-			lwm2m := &appsv1beta3.EmqxPlugin{
-				TypeMeta: metav1.TypeMeta{
-					APIVersion: "apps.emqx.io/v1beta3",
-					Kind:       "EmqxPlugin",
-				},
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      fmt.Sprintf("%s-%s", emqx.GetName(), "lwm2m"),
-					Namespace: emqx.GetNamespace(),
-					Labels:    emqx.GetLabels(),
-				},
-				Spec: appsv1beta3.EmqxPluginSpec{
-					PluginName: "emqx_lwm2m",
-					Selector:   emqx.GetLabels(),
-					Config: map[string]string{
-						"lwm2m.lifetime_min": "1s",
-						"lwm2m.lifetime_max": "86400s",
-						"lwm2m.bind.udp.1":   "0.0.0.0:5683",
-						"lwm2m.bind.udp.2":   "0.0.0.0:5684",
-						"lwm2m.bind.dtls.1":  "0.0.0.0:5685",
-						"lwm2m.bind.dtls.2":  "0.0.0.0:5686",
-						"lwm2m.xml_dir":      "/opt/emqx/etc/lwm2m_xml",
-					},
-				},
-			}
-			Expect(k8sClient.Create(context.Background(), lwm2m)).Should(Succeed())
-
-			Eventually(func() bool {
-				_ = k8sClient.Get(context.Background(), types.NamespacedName{Name: lwm2m.GetName(), Namespace: lwm2m.GetNamespace()}, lwm2m)
-				return lwm2m.Status.Phase == appsv1beta3.EmqxPluginStatusLoaded
-			}, timeout, interval).Should(BeTrue())
-
-			emqxReady <- "ready"
-		}(emqx)
-	}
-
-	// wait emqx custom resource ready
-	_, _ = <-emqxReady, <-emqxReady
 })
 
 var _ = AfterSuite(func() {
-	cleanAll()
-
 	By("tearing down the test environment")
 	Expect(testEnv.Stop()).NotTo(HaveOccurred())
 })
-
-func cleanAll() {
-	Expect(removePluginsFinalizer(broker.GetNamespace())).Should(Succeed())
-	Expect(removePluginsFinalizer(enterprise.GetNamespace())).Should(Succeed())
-
-	Expect(k8sClient.Delete(context.Background(), broker)).Should(Succeed())
-	Expect(k8sClient.Delete(context.Background(), enterprise)).Should(Succeed())
-
-	Expect(k8sClient.Delete(context.Background(), &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: broker.GetNamespace()}})).Should(Succeed())
-	Expect(k8sClient.Delete(context.Background(), &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: enterprise.GetNamespace()}})).Should(Succeed())
-
-	Eventually(func() bool {
-		err := k8sClient.Get(context.Background(), types.NamespacedName{Name: broker.GetNamespace()}, &corev1.Namespace{})
-		return k8sErrors.IsNotFound(err)
-	}, timeout, interval).Should(BeTrue())
-
-	Eventually(func() bool {
-		err := k8sClient.Get(context.Background(), types.NamespacedName{Name: enterprise.GetNamespace()}, &corev1.Namespace{})
-		return k8sErrors.IsNotFound(err)
-	}, timeout, interval).Should(BeTrue())
-}
-
-func removePluginsFinalizer(namespace string) error {
-	finalizer := "apps.emqx.io/finalizer"
-
-	plugins := &appsv1beta3.EmqxPluginList{}
-	_ = k8sClient.List(
-		context.Background(),
-		plugins,
-		client.InNamespace(namespace),
-	)
-	for _, plugin := range plugins.Items {
-		controllerutil.RemoveFinalizer(&plugin, finalizer)
-		err := k8sClient.Update(context.Background(), &plugin)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
