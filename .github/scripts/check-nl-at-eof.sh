511cb7b2b8f2569ec41777c44f142009a302c39e
Rory Z <rory-z@outlook.com>
2023-02-07 17:42:13 +0800

    chore(CI): check line-break at EOF

---
+++ .github/scripts/check-nl-at-eof.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cd -P -- "$(dirname -- "$0")/.."
+
+nl_at_eof() {
+    local file="$1"
+    if ! [ -f "$file" ]; then
+        return
+    fi
+    case "$file" in
+        *.png|*.jpg)
+            return
+            ;;
+    esac
+    local lastbyte
+    lastbyte="$(tail -c 1 "$file" 2>&1)"
+    if [ "$lastbyte" != '' ]; then
+        echo "$file"
+        return 1
+    fi
+}
+
+n=0
+while read -r file; do
+    if ! nl_at_eof "$file"; then
+        echo "nl_at_eof: $file"
+        n=$(( n + 1 ))
+    fi
+done < <(git ls-files)
+
+exit $n
