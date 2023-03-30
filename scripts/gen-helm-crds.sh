91bd52c320b04fddcbc2911b03fd3d383b2956d9
Rory Z <rory-z@outlook.com>
2023-03-30 17:20:03 +0800

    chore: add script for generate helm template crds
    - if crd doesn't have cert-manager annotation, don't set conversion
      webhook

---
+++ scripts/gen-helm-crds.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# ensure dir
+project_dir="$(dirname "$(readlink -f "$0")")"/..
+cd -P -- "$project_dir"
+
+# ensure kustomize
+test -s "$project_dir/bin/kustomize" || make kustomize -C "$project_dir"
+
+dir=$(mktemp -d)
+# generate CRDs
+pushd "$dir"
+"$project_dir/bin/kustomize" build "$project_dir/config/crd" > crds.yaml
+yq -s '"crd." + .metadata.name + ".yaml"' crds.yaml
+popd
+
+while IFS= read -r -d '' file
+do
+	if yq '.metadata.annotations' "$file" | yq 'keys' | grep -q 'cert-manager.io/inject-ca-from' > /dev/null 2>&1 ; then
+	    yq -i '.metadata.annotations."cert-manager.io/inject-ca-from" = "{{ .Release.Namespace }}/{{ include \"emqx-operator.fullname\" . }}-serving-cert"' "$file"
+	    yq -i '.spec.conversion.webhook.clientConfig.service.name= "{{ include \"emqx-operator.fullname\" . }}-webhook-service"' "$file"
+	    yq -i '.spec.conversion.webhook.clientConfig.service.namespace = "{{ .Release.Namespace }}"' "$file"
+    fi
+
+    sed -i '1i {{- if not .Values.skipCRDs }}\n' "$file"
+	echo -e '\n{{- end }}' >> "$file"
+done <   <(find "$dir" -depth -type f -name "*.yaml" ! -name "crds.yaml" -print0)
+
+find "$dir" -depth -type f -name "*.yaml" ! -name "crds.yaml" -exec mv {} "$project_dir/deploy/charts/emqx-operator/templates" \;
