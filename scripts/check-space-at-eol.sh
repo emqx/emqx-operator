3b7761f3da228308c8307b659d881809c6ff001a
Rory Z <rory-z@outlook.com>
2023-05-05 17:14:13 +0800

    ci: more better check space at end of line

--- scripts/check-space-at-eol.sh
+++ scripts/check-space-at-eol.sh
@@ -7,7 +7,8 @@ cd -P -- "$(dirname -- "$0")/.."
 n=0
 while read -r file; do
     if grep -E -q " +$" "$file"; then
-        echo "space_at_eol: $file"
+        lineNum=$(grep -n -E " +$" "$file" | cut -d: -f1 | tr '\n' ','  | sed 's/,$//')
+        echo "have space at end of $lineNum line in $file"
         n=$(( n + 1 ))
     fi
 done < <(git ls-files | grep -v -e LICENSES -e '\.png$')
