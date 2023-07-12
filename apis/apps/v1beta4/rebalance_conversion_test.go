117940e9cd28f74ce13b0a63586c2fc848af1bb9
jacky-xbb <jacky-xbb@gmail.com>
2023-07-12 19:09:38 +0800

    chore: add webhook and conversion

---
+++ apis/apps/v1beta4/rebalance_conversion_test.go
@@ -0,0 +1,38 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta4
+
+import (
+	"testing"
+
+	"github.com/emqx/emqx-operator/apis/apps/v2alpha2"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConvertTo(t *testing.T) {
+	dst := &v2alpha2.Rebalance{}
+	src := &Rebalance{}
+
+	assert.Nil(t, src.ConvertTo(dst))
+}
+
+func TestConvertFrom(t *testing.T) {
+	dst := &Rebalance{}
+	src := &v2alpha2.Rebalance{}
+
+	assert.Nil(t, dst.ConvertFrom(src))
+}
