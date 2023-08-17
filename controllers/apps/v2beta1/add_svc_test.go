1f91b1d44f7e0eea607bb48159050cac267f2439
Rory Z <16801068+Rory-Z@users.noreply.github.com>
2023-08-17 14:13:44 +0800

    chore: change svc template type
    Signed-off-by: Rory Z <16801068+Rory-Z@users.noreply.github.com>

--- controllers/apps/v2beta1/add_svc_test.go
+++ controllers/apps/v2beta1/add_svc_test.go
@@ -73,7 +73,7 @@ func TestGenerateDashboardService(t *testing.T) {
 					Labels: appsv2beta1.DefaultCoreLabels(emqx),
 				},
 			},
-			DashboardServiceTemplate: &corev1.Service{
+			DashboardServiceTemplate: &appsv2beta1.ServiceTemplate{
 				ObjectMeta: metav1.ObjectMeta{
 					Name: "emqx-dashboard",
 					Labels: map[string]string{
