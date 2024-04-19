3f902ddd20ba670d09812c9ba741bd7fa41bbaaa
Rory Z <16801068+Rory-Z@users.noreply.github.com>
2024-04-19 16:20:57 +0800

    fix: support pdb in kube 1.20
    Signed-off-by: Rory Z <16801068+Rory-Z@users.noreply.github.com>

--- controllers/apps/v2beta1/add_pdb.go
+++ controllers/apps/v2beta1/add_pdb.go
@@ -4,14 +4,19 @@ import (
 	"context"
 
 	emperror "emperror.dev/errors"
+	semver "github.com/Masterminds/semver/v3"
 	appsv2beta1 "github.com/emqx/emqx-operator/apis/apps/v2beta1"
 	innerReq "github.com/emqx/emqx-operator/internal/requester"
 	"github.com/go-logr/logr"
 	policyv1 "k8s.io/api/policy/v1"
+	policyv1beta1 "k8s.io/api/policy/v1beta1"
 	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/client-go/discovery"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	kubeConfig "sigs.k8s.io/controller-runtime/pkg/client/config"
 )
 
 type addPdb struct {
@@ -19,7 +24,25 @@ type addPdb struct {
 }
 
 func (a *addPdb) reconcile(ctx context.Context, logger logr.Logger, instance *appsv2beta1.EMQX, _ innerReq.RequesterInterface) subResult {
-	pdbList := generatePodDisruptionBudget(instance)
+	discoveryClient, _ := discovery.NewDiscoveryClientForConfig(kubeConfig.GetConfigOrDie())
+	kubeVersion, _ := discoveryClient.ServerVersion()
+	v, _ := semver.NewVersion(kubeVersion.String())
+
+	pdbList := []client.Object{}
+	if v.LessThan(semver.MustParse("1.21")) {
+		corePdb, replPdb := generatePodDisruptionBudgetV1beta1(instance)
+		pdbList = append(pdbList, corePdb)
+		if replPdb != nil {
+			pdbList = append(pdbList, replPdb)
+		}
+	} else {
+		corePdb, replPdb := generatePodDisruptionBudget(instance)
+		pdbList = append(pdbList, corePdb)
+		if replPdb != nil {
+			pdbList = append(pdbList, replPdb)
+		}
+	}
+
 	for _, pdb := range pdbList {
 		if err := ctrl.SetControllerReference(instance, pdb, a.Scheme); err != nil {
 			return subResult{err: emperror.Wrap(err, "failed to set controller reference")}
@@ -33,8 +56,7 @@ func (a *addPdb) reconcile(ctx context.Context, logger logr.Logger, instance *ap
 	return subResult{}
 }
 
-func generatePodDisruptionBudget(instance *appsv2beta1.EMQX) []*policyv1.PodDisruptionBudget {
-	pdbList := []*policyv1.PodDisruptionBudget{}
+func generatePodDisruptionBudget(instance *appsv2beta1.EMQX) (*policyv1.PodDisruptionBudget, *policyv1.PodDisruptionBudget) {
 	corePdb := &policyv1.PodDisruptionBudget{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "policy/v1",
@@ -58,7 +80,6 @@ func generatePodDisruptionBudget(instance *appsv2beta1.EMQX) []*policyv1.PodDisr
 			},
 		},
 	}
-	pdbList = append(pdbList, corePdb)
 	if appsv2beta1.IsExistReplicant(instance) {
 		replPdb := corePdb.DeepCopy()
 		replPdb.Name = instance.ReplicantNamespacedName().Name
@@ -66,7 +87,43 @@ func generatePodDisruptionBudget(instance *appsv2beta1.EMQX) []*policyv1.PodDisr
 			appsv2beta1.DefaultReplicantLabels(instance),
 			instance.Spec.ReplicantTemplate.Labels,
 		)
-		pdbList = append(pdbList, replPdb)
+		return corePdb, replPdb
 	}
-	return pdbList
+	return corePdb, nil
+}
+
+func generatePodDisruptionBudgetV1beta1(instance *appsv2beta1.EMQX) (*policyv1beta1.PodDisruptionBudget, *policyv1beta1.PodDisruptionBudget) {
+	corePdb := &policyv1beta1.PodDisruptionBudget{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "policy/v1",
+			Kind:       "PodDisruptionBudget",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: instance.Namespace,
+			Name:      instance.CoreNamespacedName().Name,
+			Labels:    appsv2beta1.CloneAndMergeMap(appsv2beta1.DefaultLabels(instance), instance.Labels),
+		},
+		Spec: policyv1beta1.PodDisruptionBudgetSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: appsv2beta1.CloneAndMergeMap(
+					appsv2beta1.DefaultCoreLabels(instance),
+					instance.Spec.CoreTemplate.Labels,
+				),
+			},
+			MinAvailable: &intstr.IntOrString{
+				Type:   intstr.Int,
+				IntVal: 1,
+			},
+		},
+	}
+	if appsv2beta1.IsExistReplicant(instance) {
+		replPdb := corePdb.DeepCopy()
+		replPdb.Name = instance.ReplicantNamespacedName().Name
+		replPdb.Spec.Selector.MatchLabels = appsv2beta1.CloneAndMergeMap(
+			appsv2beta1.DefaultReplicantLabels(instance),
+			instance.Spec.ReplicantTemplate.Labels,
+		)
+		return corePdb, replPdb
+	}
+	return corePdb, nil
 }
