20ba9d3063e0213379af99c2238810e64da4f35f
Andrew Maiorov <encube.ul@gmail.com>
2026-04-15 17:23:35 +0200

    fix: make sure `podsManagedBy{nil}` evaluates safely

--- internal/controller/load_state.go
+++ internal/controller/load_state.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"context"
+	"reflect"
 
 	emperror "emperror.dev/errors"
 	crd "github.com/emqx/emqx-operator/api/v3alpha1"
@@ -28,11 +29,14 @@ type reconcileStatePodFilter interface {
 }
 
 type podsManagedBy struct {
-	metav1.Object
+	manager metav1.Object
 }
 
 func (self podsManagedBy) passes(pod *corev1.Pod) bool {
-	return self.Object != nil && util.IsPodManagedBy(pod, self.Object)
+	if self.manager == nil && reflect.ValueOf(self.manager).IsNil() {
+		return false
+	}
+	return util.IsPodManagedBy(pod, self.manager)
 }
 
 type podsWithRole struct {
