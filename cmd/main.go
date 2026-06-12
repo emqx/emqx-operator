2e8de4f8763ee4879ae4f42d84ccb453e5c76b23
Andrew Maiorov <encube.ul@gmail.com>
2026-06-12 15:29:01 +0200

    chore(rbac): drop persistentvolumes permissions
    This commits drops unused permission, as Operator does not work with
    PVs directly anymore.
    
    (cherry picked from commit 3d65a9323e547edbbecc689795090ba2b6869e5d)

--- cmd/main.go
+++ cmd/main.go
@@ -65,7 +65,6 @@ func init() {
 
 // +kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch;create;update
 // +kubebuilder:rbac:groups="",resources=events,verbs=get;list;watch;create;update;patch
-// +kubebuilder:rbac:groups="",resources=persistentvolumes,verbs=get;list;watch;create;update
 // +kubebuilder:rbac:groups="",resources=persistentvolumeclaims,verbs=get;list;watch;create;update;delete
 // +kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch;update;delete
 // +kubebuilder:rbac:groups="",resources=pods/exec,verbs=create
