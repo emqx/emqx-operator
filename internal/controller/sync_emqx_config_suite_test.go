288d88f655ae9b57945447f29cc79f1205bc2522
Andrew Maiorov <encube.ul@gmail.com>
2026-06-14 18:06:24 +0200

    test: streamline syncConfig suite

--- internal/controller/sync_emqx_config_suite_test.go
+++ internal/controller/sync_emqx_config_suite_test.go
@@ -4,21 +4,21 @@ import (
 	"net/http"
 	"net/url"
 
-	crdv2 "github.com/emqx/emqx-operator/api/v2"
+	crd "github.com/emqx/emqx-operator/api/v3beta1"
 	config "github.com/emqx/emqx-operator/internal/controller/config"
 	resources "github.com/emqx/emqx-operator/internal/controller/resources"
 	req "github.com/emqx/emqx-operator/internal/requester"
 	"github.com/lithammer/dedent"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 var _ = Describe("Reconciler syncConfig", Ordered, func() {
 	var ns *corev1.Namespace
-	var instance *crdv2.EMQX
+	var instance *crd.EMQX
 	var s *syncConfig
 
 	BeforeEach(func() {
@@ -36,7 +36,7 @@ var _ = Describe("Reconciler syncConfig", Ordered, func() {
 		`)
 		Expect(k8sClient.Create(ctx, instance)).Should(Succeed())
 
-		s = &syncConfig{emqxReconciler}
+		s = &syncConfig{emqxReconciler()}
 	})
 
 	AfterEach(func() {
@@ -45,20 +45,18 @@ var _ = Describe("Reconciler syncConfig", Ordered, func() {
 
 	It("creates the ConfigMap and records last-applied config", func() {
 		Eventually(s.reconcile).WithArguments(newReconcileRound(), instance).
-			WithTimeout(timeout).
-			WithPolling(interval).
-			Should(Equal(subResult{}))
+			Should(BeSuccessfulReconcile())
 
 		configMap := actualConfigMap(instance)
-		Expect(configMap.Data).To(HaveKeyWithValue(
-			resources.BaseConfigFile,
-			config.WithDefaults(instance.Spec.Config.Data),
-		))
-		Expect(configMap.Data).To(HaveKeyWithValue(resources.OverridesConfigFile, ""))
 		Expect(configMap.OwnerReferences).To(ContainElement(HaveField("Name", Equal(instance.Name))))
+		Expect(configMap.Data).To(And(
+			HaveKeyWithValue(resources.BaseConfigFile, config.WithDefaults(instance.Spec.Config.Data)),
+			HaveKeyWithValue(resources.OverridesConfigFile, ""),
+		))
 
-		Expect(actualInstance(instance).Annotations).To(HaveKeyWithValue(
-			crdv2.AnnotationLastEMQXConfig,
+		Expect(actualize(instance)).To(Succeed())
+		Expect(instance.Annotations).To(HaveKeyWithValue(
+			crd.AnnotationLastEMQXConfig,
 			instance.Spec.Config.Data,
 		))
 	})
@@ -67,43 +65,49 @@ var _ = Describe("Reconciler syncConfig", Ordered, func() {
 		lastConfig := dedent.Dedent(`
 			listeners.tcp.default.bind = "0.0.0.0:1883"
 		`)
+		updateSpecConfig(instance, lastConfig)
+		Eventually(s.reconcile).WithArguments(newReconcileRound(), instance).
+			Should(BeSuccessfulReconcile())
+
 		nextConfig := dedent.Dedent(`
 			listeners.tcp.default.bind = "0.0.0.0:1884"
 		`)
-		instance = establishConfig(s, instance, lastConfig)
-		instance = updateSpecConfig(instance, nextConfig)
+		updateSpecConfig(instance, nextConfig)
 
-		calls := []syncConfigAPIRequest{}
-		round := newSyncConfigRound(&calls, 204)
-		Eventually(s.reconcile).WithArguments(round, instance).
-			WithTimeout(timeout).
-			WithPolling(interval).
-			Should(Equal(subResult{}))
+		intercept := mkAPIRequesterInterceptor(req.NewMockRequester(
+			func(method string, url url.URL, body []byte, _ http.Header) (*http.Response, []byte, error) {
+				return &http.Response{StatusCode: 204}, []byte{}, nil
+			},
+		))
+		Eventually(func() subResult {
+			return s.reconcile(newSyncConfigReconcileRound(0, intercept.apiRequester()), instance)
+		}).
+			Should(Equal(reconcilePostpone()))
 
 		configMap := actualConfigMap(instance)
-		Expect(configMap.Data[resources.BaseConfigFile]).To(Equal(config.WithDefaults(nextConfig)))
-		Expect(actualInstance(instance).Annotations).To(HaveKeyWithValue(
-			crdv2.AnnotationLastEMQXConfig,
-			lastConfig,
-		))
-		Expect(calls).To(BeEmpty())
+		Expect(configMap.Data).To(
+			HaveKeyWithValue(resources.BaseConfigFile, Equal(config.WithDefaults(nextConfig))),
+		)
+		Expect(intercept.listCaptured()).To(BeEmpty())
+		Expect(actualize(instance)).To(Succeed())
+		Expect(instance.Annotations).To(
+			HaveKeyWithValue(crd.AnnotationLastEMQXConfig, lastConfig),
+		)
 	})
 
 	It("strips non-changeable dashboard listener update", func() {
-		lastConfig := dedent.Dedent(`
+		updateSpecConfig(instance, dedent.Dedent(`
 			listeners.tcp.default.bind = "0.0.0.0:1883"
-		`)
-		nextConfig := dedent.Dedent(`
+		`))
+		Eventually(s.reconcile).WithArguments(newReconcileRound(), instance).
+			Should(BeSuccessfulReconcile())
+
+		updateSpecConfig(instance, dedent.Dedent(`
 			dashboard.listeners.http.bind = 18084
 			listeners.tcp.default.bind = "0.0.0.0:1884"
-		`)
-		instance = establishConfig(s, instance, lastConfig)
-		instance = updateSpecConfig(instance, nextConfig)
-
+		`))
 		Eventually(s.reconcile).WithArguments(newReconcileRound(), instance).
-			WithTimeout(timeout).
-			WithPolling(interval).
-			Should(Equal(subResult{}))
+			Should(Equal(reconcilePostpone()))
 
 		configMap := actualConfigMap(instance)
 		conf, err := config.EMQXConfig(configMap.Data[resources.BaseConfigFile])
@@ -113,21 +117,19 @@ var _ = Describe("Reconciler syncConfig", Ordered, func() {
 	})
 
 	It("strips non-changeable HTTPS dashboard listener update", func() {
-		lastConfig := dedent.Dedent(`
+		updateSpecConfig(instance, dedent.Dedent(`
 			dashboard.listeners.https.bind = 18084
 			listeners.tcp.default.bind = "0.0.0.0:1883"
-		`)
-		nextConfig := dedent.Dedent(`
+		`))
+		Eventually(s.reconcile).WithArguments(newReconcileRound(), instance).
+			Should(BeSuccessfulReconcile())
+
+		updateSpecConfig(instance, dedent.Dedent(`
 			dashboard.listeners.https.bind = 18085
 			listeners.tcp.default.bind = "0.0.0.0:1884"
-		`)
-		instance = establishConfig(s, instance, lastConfig)
-		instance = updateSpecConfig(instance, nextConfig)
-
+		`))
 		Eventually(s.reconcile).WithArguments(newReconcileRound(), instance).
-			WithTimeout(timeout).
-			WithPolling(interval).
-			Should(Equal(subResult{}))
+			Should(Equal(reconcilePostpone()))
 
 		configMap := actualConfigMap(instance)
 		conf, err := config.EMQXConfig(configMap.Data[resources.BaseConfigFile])
@@ -137,21 +139,19 @@ var _ = Describe("Reconciler syncConfig", Ordered, func() {
 	})
 
 	It("allows enabling a previously disabled dashboard listener", func() {
-		lastConfig := dedent.Dedent(`
+		updateSpecConfig(instance, dedent.Dedent(`
 			dashboard.listeners.http.bind = 0
 			listeners.tcp.default.bind = "0.0.0.0:1883"
-		`)
-		nextConfig := dedent.Dedent(`
+		`))
+		Eventually(s.reconcile).WithArguments(newReconcileRound(), instance).
+			Should(BeSuccessfulReconcile())
+
+		updateSpecConfig(instance, dedent.Dedent(`
 			dashboard.listeners.http.bind = 18084
 			listeners.tcp.default.bind = "0.0.0.0:1884"
-		`)
-		instance = establishConfig(s, instance, lastConfig)
-		instance = updateSpecConfig(instance, nextConfig)
-
+		`))
 		Eventually(s.reconcile).WithArguments(newReconcileRound(), instance).
-			WithTimeout(timeout).
-			WithPolling(interval).
-			Should(Equal(subResult{}))
+			Should(Equal(reconcilePostpone()))
 
 		configMap := actualConfigMap(instance)
 		conf, err := config.EMQXConfig(configMap.Data[resources.BaseConfigFile])
@@ -161,9 +161,13 @@ var _ = Describe("Reconciler syncConfig", Ordered, func() {
 	})
 
 	It("applies runtime config when core nodes are ready and strips readonly runtime entries", func() {
-		lastConfig := dedent.Dedent(`
+		instance.Spec.Config.Mode = "Replace"
+		updateSpecConfig(instance, dedent.Dedent(`
 			listeners.tcp.default.bind = "0.0.0.0:1883"
-		`)
+		`))
+		Eventually(s.reconcile).WithArguments(newReconcileRound(), instance).
+			Should(BeSuccessfulReconcile())
+
 		nextConfig := dedent.Dedent(`
 			node.name = "emqx@127.0.0.1"
 			rpc.tcp_client_num = 1
@@ -171,24 +175,25 @@ var _ = Describe("Reconciler syncConfig", Ordered, func() {
 			cluster.links.remote.server = "remote:1883"
 			listeners.tcp.default.bind = "0.0.0.0:1884"
 		`)
-		instance = establishConfig(s, instance, lastConfig)
-		instance = updateSpecConfig(instance, nextConfig)
-		instance.Status.SetTrueCondition(crdv2.CoreNodesReady)
+		updateSpecConfig(instance, nextConfig)
 
-		calls := []syncConfigAPIRequest{}
-		round := newSyncConfigRound(&calls, 204)
-		Eventually(s.reconcile).WithArguments(round, instance).
-			WithTimeout(timeout).
-			WithPolling(interval).
-			Should(Equal(subResult{result: ctrl.Result{Requeue: true}}))
+		intercept := mkAPIRequesterInterceptor(req.NewMockRequester(
+			func(method string, url url.URL, body []byte, _ http.Header) (*http.Response, []byte, error) {
+				return &http.Response{StatusCode: 204}, []byte{}, nil
+			},
+		))
+		Eventually(func() subResult {
+			return s.reconcile(newSyncConfigReconcileRound(1, intercept.apiRequester()), instance)
+		}).
+			Should(Equal(reconcileRequeue()))
 
-		Expect(calls).To(ConsistOf(And(
+		Expect(intercept.listCaptured()).To(ConsistOf(And(
 			HaveField("Method", Equal(http.MethodPut)),
-			HaveField("Path", Equal("api/v5/configs")),
+			HaveField("URL.Path", Equal("api/v5/configs")),
 			HaveField("Header", HaveKeyWithValue("Content-Type", ConsistOf(Equal("text/plain")))),
 		)))
 
-		runtimeConfig, err := config.EMQXConfig(calls[0].Body)
+		runtimeConfig, err := config.EMQXConfig(intercept.captured(0).Body)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(runtimeConfig.Find("node")).To(BeNil())
 		Expect(runtimeConfig.Find("rpc")).To(BeNil())
@@ -196,75 +201,61 @@ var _ = Describe("Reconciler syncConfig", Ordered, func() {
 		Expect(runtimeConfig.Find("cluster.links")).NotTo(BeNil())
 		Expect(runtimeConfig.Find("listeners")).NotTo(BeNil())
 
-		Expect(actualInstance(instance).Annotations).To(HaveKeyWithValue(
-			crdv2.AnnotationLastEMQXConfig,
-			nextConfig,
-		))
+		Expect(actualize(instance)).To(Succeed())
+		Expect(instance.Annotations).To(
+			HaveKeyWithValue(crd.AnnotationLastEMQXConfig, nextConfig),
+		)
 	})
 
 	It("returns an error and keeps last-applied config when runtime update fails", func() {
 		lastConfig := dedent.Dedent(`
 			listeners.tcp.default.bind = "0.0.0.0:1883"
 		`)
+		updateSpecConfig(instance, lastConfig)
+		Eventually(s.reconcile).WithArguments(newReconcileRound(), instance).
+			Should(BeSuccessfulReconcile())
+
 		nextConfig := dedent.Dedent(`
 			listeners.tcp.default.bind = "0.0.0.0:1884"
 		`)
-		instance = establishConfig(s, instance, lastConfig)
-		instance = updateSpecConfig(instance, nextConfig)
-		instance.Status.SetTrueCondition(crdv2.CoreNodesReady)
+		updateSpecConfig(instance, nextConfig)
 
-		calls := []syncConfigAPIRequest{}
-		result := s.reconcile(newSyncConfigRound(&calls, 500), instance)
-
-		Expect(result.result).To(Equal(ctrl.Result{}))
-		Expect(result.err).To(MatchError(ContainSubstring("failed to update emqx config through API")))
-		Expect(calls).To(HaveLen(1))
-		Expect(actualInstance(instance).Annotations).To(HaveKeyWithValue(
-			crdv2.AnnotationLastEMQXConfig,
-			lastConfig,
+		intercept := mkAPIRequesterInterceptor(req.NewMockRequester(
+			func(method string, url url.URL, body []byte, _ http.Header) (*http.Response, []byte, error) {
+				return &http.Response{StatusCode: 500}, []byte("runtime update failed"), nil
+			},
 		))
+		Eventually(func() subResult {
+			return s.reconcile(newSyncConfigReconcileRound(1, intercept.apiRequester()), instance)
+		}).
+			Should(BeReconcileError(
+				MatchError(ContainSubstring("failed to update emqx config through API")),
+			))
+
+		Expect(intercept.listCaptured()).To(HaveLen(1))
+		Expect(actualize(instance)).To(Succeed())
+		Expect(instance.Annotations).To(
+			HaveKeyWithValue(crd.AnnotationLastEMQXConfig, lastConfig),
+		)
 	})
 })
 
-type syncConfigAPIRequest struct {
-	Method string
-	Path   string
-	Body   string
-	Header http.Header
+func newSyncConfigReconcileRound(readyReplicas int32, requester apiRequester) *reconcileRound {
+	round := newReconcileRound()
+	round.state.coreSets = []*appsv1.StatefulSet{{
+		Status: appsv1.StatefulSetStatus{ReadyReplicas: readyReplicas},
+	}}
+	round.requester = requester
+	return round
 }
 
-func newSyncConfigRound(calls *[]syncConfigAPIRequest, statusCode int) *reconcileRound {
-	requester := req.NewMockRequester(
-		func(method string, u url.URL, body []byte, header http.Header) (*http.Response, []byte, error) {
-			*calls = append(*calls, syncConfigAPIRequest{
-				Method: method,
-				Path:   u.Path,
-				Body:   string(body),
-				Header: header,
-			})
-			return &http.Response{StatusCode: statusCode}, []byte("runtime update failed"), nil
-		},
-	)
-	return newReconcileRoundWithRequester(requester)
-}
-
-func establishConfig(s *syncConfig, instance *crdv2.EMQX, configData string) *crdv2.EMQX {
+func updateSpecConfig(instance *crd.EMQX, configData string) {
 	instance.Spec.Config.Data = configData
 	Expect(k8sClient.Update(ctx, instance)).Should(Succeed())
-	Eventually(s.reconcile).WithArguments(newReconcileRound(), actualInstance(instance)).
-		WithTimeout(timeout).
-		WithPolling(interval).
-		Should(Equal(subResult{}))
-	return actualInstance(instance)
+	Expect(actualize(instance)).To(Succeed())
 }
 
-func updateSpecConfig(instance *crdv2.EMQX, configData string) *crdv2.EMQX {
-	instance.Spec.Config.Data = configData
-	Expect(k8sClient.Update(ctx, instance)).Should(Succeed())
-	return actualInstance(instance)
-}
-
-func actualConfigMap(instance *crdv2.EMQX) *corev1.ConfigMap {
+func actualConfigMap(instance *crd.EMQX) *corev1.ConfigMap {
 	configMap := &corev1.ConfigMap{}
 	Expect(k8sClient.Get(ctx, instance.ConfigsNamespacedName(), configMap)).Should(Succeed())
 	return configMap
